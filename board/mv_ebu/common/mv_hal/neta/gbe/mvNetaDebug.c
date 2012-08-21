919978c2fbf733bdfb1986bc47d75075e9b1693c
Tawfik Bayouk <tawfik@marvell.com>
2012-08-21 17:35:42 +0300

    replace printk() with mvOsPrintf() - fix done by (Rami R. on 3.0.y kernel)
    Change-Id: Ib4c89f333b9bcc74ebfeafecdd996c1b344c394c
    Signed-off-by: Tawfik Bayouk <tawfik@marvell.com>

--- board/mv_ebu/common/mv_hal/neta/gbe/mvNetaDebug.c
+++ board/mv_ebu/common/mv_hal/neta/gbe/mvNetaDebug.c
@@ -770,8 +770,8 @@ void mvNetaCpuDump(int port, int cpu, int rxTx)
        static const char  *qType[] = {"RXQ", "TXQ"};
 
        if (rxTx > 1 || rxTx < 0) {
-               printk(KERN_ERR "%s: Error - invalid queue type %d , valid values are 0 for TXQ or 1 for RXQ\n", __func__, rxTx);
-               return;
+		OsPrintf("Error - invalid queue type %d , valid values are 0 for TXQ or 1 for RXQ\n", rxTx);
+		return;
        }
 
        if (rxTx == 1)

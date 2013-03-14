24dbed9654c5ad6ac349f0390abfd3df7328128d
Omri Itach <omrii@marvell.com>
2013-03-14 16:22:47 +0200

    Bump U-Boot 2013.01 2013_Q2.1_eng_drop
    Change-Id: I5d58754951b544ee9a037a88cb32405f6f3ffe60
    Reviewed-on: http://vgitil04.il.marvell.com:8080/1303
    Reviewed-by: Omri Itach <omrii@marvell.com>
    Tested-by: Omri Itach <omrii@marvell.com>

--- include/configs/avanta_lp.h
+++ include/configs/avanta_lp.h
@@ -55,7 +55,7 @@ disclaimer.
 /*
  * Version
  */
-#define CONFIG_IDENT_STRING		" Marvell version: 2013_Q2.0_eng_drop"
+#define CONFIG_IDENT_STRING		" Marvell version: 2013_Q2.1_eng_drop"
 
 /* Version number passed to kernel */
 #define VER_NUM 0x11120000		/* 2011.12 */

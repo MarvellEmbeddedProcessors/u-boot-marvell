8f9d33807cd3aaf1b405fb5d726472656dc0b1d8
Omri Itach <omrii@marvell.com>
2013-06-23 19:15:23 +0300

    NAND: Fixed Bootrom false setting to SoC_Device_Multiplex
    	- Set NfArbiterEn (BIT 27) to NAND Flash
    
    Change-Id: If0381ab4367f135ca884dffaf14a6c1df149ff6d
    Signed-off-by: Omri Itach <omrii@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/2332
    Reviewed-by: Star_Automation <star@marvell.com>
    Reviewed-by: Eran Ben-Avi <benavi@marvell.com>
    Tested-by: Eran Ben-Avi <benavi@marvell.com>

--- board/mv_ebu/common/USP/nand_nfc.c
+++ board/mv_ebu/common/USP/nand_nfc.c
@@ -1693,13 +1693,18 @@ int board_nand_init(struct nand_chip *nand)
 	struct orion_nfc_info *info;
 	int ret = 0;
 	MV_NFC_INFO nfcInfo;
+	unsigned int muxReg;
 
 	info = malloc(sizeof(struct orion_nfc_info));
 	if (info == NULL) {
 		dev_err(&pdev->dev, "orion_nfc_info malloc failed!\n");
 		return -ENOMEM;
 	}
-	MV_REG_BIT_SET(MV_SOC_DEV_MUX_REG, BIT0);
+	/* Set NfArbiterEn to NAND Flash (Bootrom accidently Set NfArbiterEn to Device) */
+	muxReg = MV_REG_READ(MV_SOC_DEV_MUX_REG);
+	muxReg |= BIT0;
+	muxReg &= ~BIT27;
+	MV_REG_WRITE(MV_SOC_DEV_MUX_REG, muxReg);
 
 	info->tclk = CONFIG_SYS_TCLK;
 #ifdef MV_NAND_GANG_MODE

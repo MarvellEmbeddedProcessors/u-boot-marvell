b257588d9e204c347284a75d3eda215fb55b621d
Omri Itach <omrii@marvell.com>
2013-06-24 14:37:10 +0300

    alp: mpp & gpio: Update RD-6650 board MPP and GPIO values
    	- Updated RD-6650 board GPP and MPP values, according to MPP Map from design
    
    Change-Id: I64f18e8236d1e446a8dd0f00552fefa27788eff9
    Signed-off-by: Omri Itach <omrii@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/2336
    Reviewed-by: Star_Automation <star@marvell.com>
    Reviewed-by: Kosta Zertsekel <konszert@marvell.com>
    Reviewed-by: Nadav Haklai <nadavh@marvell.com>
    Tested-by: Nadav Haklai <nadavh@marvell.com>

--- board/mv_ebu/alp/avanta_lp_family/boardEnv/mvBoardEnvSpec.h
+++ board/mv_ebu/alp/avanta_lp_family/boardEnv/mvBoardEnvSpec.h
@@ -160,18 +160,18 @@
 *******************************************************************************/
 #define RD_88F6650_MPP0_7               0x55555555  /* NAND */
 #define RD_88F6650_MPP8_15              0x22555555  /* NAND , I2C */
-#define RD_88F6650_MPP16_23             0x00000022
-#define RD_88F6650_MPP24_31             0x22222222  /* RGMII-1*/
-#define RD_88F6650_MPP32_39             0x04422222  /* GE_SMI ,GE1, PON */
-#define RD_88F6650_MPP40_47             0x22100220  /* PON , GE0 */
+#define RD_88F6650_MPP16_23             0x11110022  /* UA0, SSI, GPIO's */
+#define RD_88F6650_MPP24_31             0x04606101  /* PCIe0CLKREQ, P0,C0_Leds & DYING GASP*/
+#define RD_88F6650_MPP32_39             0x04420000  /* GE_SMI, PON & GPIO's */
+#define RD_88F6650_MPP40_47             0x22120020  /* GE0, PON_[XVR,TX_PD,TX_SD]*/
 #define RD_88F6650_MPP48_55             0x22222222  /* GE0*/
-#define RD_88F6650_MPP56_63             0x44444422  /* GE0 , LED */
-#define RD_88F6650_MPP64_67             0x004
+#define RD_88F6650_MPP56_63             0x44004422  /* GE0 , LED, GPIO's */
+#define RD_88F6650_MPP64_67             0x004	    /* GPIO's */
 
-#define RD_88F6650_GPP_OUT_ENA_LOW      0xFFFFFFFF
+#define RD_88F6650_GPP_OUT_ENA_LOW      (~(BIT19 | BIT25))
 #define RD_88F6650_GPP_OUT_ENA_MID      0xFFFFFFFF
 #define RD_88F6650_GPP_OUT_ENA_HIGH     0xFFFFFFFF
-#define RD_88F6650_GPP_OUT_VAL_LOW      0x0
+#define RD_88F6650_GPP_OUT_VAL_LOW      (BIT19 | BIT25)
 #define RD_88F6650_GPP_OUT_VAL_MID      0x0
 #define RD_88F6650_GPP_OUT_VAL_HIGH     0x0
 #define RD_88F6650_GPP_POL_LOW          0x0

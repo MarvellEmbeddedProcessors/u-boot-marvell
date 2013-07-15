7317c714569124e8fbf8194d6d3da14a51243a2e
Shadi Ammouri <shadi@marvell.com>
2013-07-15 15:48:28 +0300

    stage_boot: add support for sdio and usb scanning
    	- add sdio and usb initialization
    	- improve checking conditions in scanning ide for script
    	- extend conditions in ide scanning steps
    	- enable writing to 'boot_source_device' env variable a device's name, number
    	  and partition after a script is successfully loaded from it
    	- modify command's description
    	- enable automatic search for devices' amount and their numbers
    	- interface scan is begun from device and partition number read from
    	  'device partition' variable
    	- improve memory allocation for path_to_initrd, path_to_image, path_to_scr
    	  (previously it was not including '0' string termination)
    
    	Signed-off-by: Marcin Wojtas <mw@semihalf.com>
    
    Change-Id: I6375f3c25cd28ede8f834e97af1a6f3be1af4df8
    Reviewed-on: http://vgitil04.il.marvell.com:8080/2679
    Reviewed-by: Omri Itach <omrii@marvell.com>
    Tested-by: Omri Itach <omrii@marvell.com>

--- board/mv_ebu/axp/cmd_stage_boot.c
+++ board/mv_ebu/axp/cmd_stage_boot.c
@@ -64,6 +64,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <common.h>
 #include <command.h>
 #include <net.h>
+#include <usb.h>
+#include <mmc.h>
+#include <ide.h>
 #if defined(CONFIG_CMD_STAGE_BOOT)
 
 #ifdef CONFIG_SYS_HUSH_PARSER
@@ -73,8 +76,13 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define LOAD_ADDR getenv("script_addr_r")
 #define SCRIPT_PATH getenv("ide_path")
 #define INTERFACE_HD "ide"
+#define INTERFACE_MMC "mmc"
+#define INTERFACE_USB "usb"
 #define FLAG_EXIT_FROM_LOOP 1
 #define FLAG_PARSE_SEMICOLON (1 << 1)	  /* symbol ';' is special for parser */
+#define DEFAULT_DEVICE_NUMBER 0
+#define DEFAULT_PART_NUMBER 1
+#define MAX_MMC_DEV 1
 	
 char enviroment[10][22];
 
@@ -84,6 +92,8 @@ extern int do_pxe_boot(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]
 extern int do_pxe_get(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]);
 extern int do_dhcp (cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]);
 extern int do_ext2load (cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]);
+extern int do_mmcops(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]);
+extern int do_usb(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]);
 extern void * malloc(__kernel_size_t);
 extern int free(void *);
 void save_env (void);
@@ -91,50 +101,118 @@ void restore_env(void);
 
 int do_stage_boot(cmd_tbl_t * cmdtb, int flag, int argc, char * const argv[])
 {
-	char *path_to_image, *path_to_scr, *path_to_initrd, *all_partions;
+	char *path_to_image, *path_to_scr, *path_to_initrd;
         ulong addr;
-	int j,i=1,step=0,len,index,device,initrd=0;
+	int j,i=1,step=0,device,initrd=0,devnum,partnum,dev_count,max_dev_count;
 	char * kernel_addr, *bootargs;
+	char dev_part[6], *dev_part_ptr;
         char * args_to_func[5];
-	char device_prt[4];
+	char boot_source_device[8];
+	block_dev_desc_t * dev_desc;
 
 	if(argc < 2){
 		printf("No args, try help stage_boot\n");
 		return 0;
 	}
-	path_to_initrd = (char *)malloc((strlen(SCRIPT_PATH)+strlen(getenv("initrd_name")))*sizeof(char));
+	path_to_initrd = (char *)malloc((strlen(SCRIPT_PATH)+strlen(getenv("initrd_name"))+1)*sizeof(char));
 	strcpy(path_to_initrd,SCRIPT_PATH);
 	strcat(path_to_initrd,getenv("initrd_name"));
-	path_to_image = (char *)malloc((strlen(SCRIPT_PATH)+strlen(getenv("image_name")))*sizeof(char));
+	path_to_image = (char *)malloc((strlen(SCRIPT_PATH)+strlen(getenv("image_name"))+1)*sizeof(char));
 	strcpy(path_to_image,SCRIPT_PATH);
 	strcat(path_to_image,getenv("image_name"));
-	path_to_scr = (char *)malloc((strlen(SCRIPT_PATH)+strlen(getenv("script_name")))*sizeof(char));
+	path_to_scr = (char *)malloc((strlen(SCRIPT_PATH)+strlen(getenv("script_name"))+1)*sizeof(char));
 	strcpy(path_to_scr,SCRIPT_PATH);
 	strcat(path_to_scr,getenv("script_name"));
 
-
-	all_partions = (char *)malloc(strlen(getenv("device_partition"))*sizeof(char));
-	strcpy(all_partions,getenv("device_partition"));
 	kernel_addr = getenv("kernel_addr_r");
 
+	/* initialize interfaces */
 	ide_init();
+	args_to_func[0]="usb";
+	args_to_func[1]="start";
+	do_usb(cmdtb, 1, 2, args_to_func);
+	args_to_func[0]="mmc";
+	args_to_func[1]="rescan";
+	do_mmcops(cmdtb, 1, 2, args_to_func);
+
+	/* get device and partition number from 'device_partition' variable */
+	dev_part_ptr = getenv("device_partition");
+	if (dev_part_ptr == NULL)
+	{
+		devnum = DEFAULT_DEVICE_NUMBER;
+		printf("'device_partition' variable not set"
+		    ", using default device number "
+		    "%d\n", devnum);
+		partnum = DEFAULT_PART_NUMBER;
+		printf("'device_partition' variable not set"
+		    ", using default partition number "
+		    "%d\n", partnum);
+	}
+	else
+	{
+		devnum = simple_strtoul(dev_part_ptr, NULL, 0);
+		dev_part_ptr = strchr(dev_part_ptr, ':');
+		if (dev_part_ptr == NULL)
+		{
+			devnum = DEFAULT_DEVICE_NUMBER;
+			printf("'device_partition' variable set "
+			    "incorrectly, using default device number "
+			    "%d\n", devnum);
+		}
+		dev_part_ptr++;
+		partnum = simple_strtoul(dev_part_ptr, NULL, 0);
+		if (partnum == 0)
+		{
+			partnum = DEFAULT_PART_NUMBER;
+			printf("'device_partition' variable set "
+			    "incorrectly, using default partition number "
+			    "%d\n", partnum);
+		}
+	}
+
 	for(device=1;device<argc;device++)
 	{
-		/* step 1 load script from ide */
-		len=strlen(all_partions);
-		for(index=0;index<len && i==1 && strcmp(argv[device],"hd_scr")==0 ;index++)
+		/* step 1 load script from storage device */
+		if( i == 1 && (strcmp(argv[device],"hd_scr")==0 ||
+		    strcmp(argv[device],"mmc_scr")==0 ||
+		    strcmp(argv[device],"usb_scr")==0))
 		{
 			step=1;
-			for(j=0;j<3;j++,index++)
-				device_prt[j]=all_partions[index];
-			device_prt[3]='\0';
-			printf("\ntry to load script from ide %s\n",device_prt);
+			dev_count=0;
 			args_to_func[0]="ext2load";
-			args_to_func[1]=INTERFACE_HD;
-			args_to_func[2]=device_prt;
 			args_to_func[3]=LOAD_ADDR;
 			args_to_func[4]=path_to_scr;
-			i = do_ext2load(cmdtb, 1, 5 , args_to_func);
+			if (strcmp(argv[device],"hd_scr") == 0)
+			{
+				args_to_func[1] = INTERFACE_HD;
+				max_dev_count = CONFIG_SYS_IDE_MAXDEVICE;
+			}
+			else if (strcmp(argv[device],"mmc_scr") == 0)
+			{
+				args_to_func[1] = INTERFACE_MMC;
+				max_dev_count = MAX_MMC_DEV;
+			}
+			else
+			{
+				args_to_func[1] = INTERFACE_USB;
+				max_dev_count = USB_MAX_STOR_DEV;
+			}
+			printf("\nscanning %s devices beginning from: %d:%d\n",
+			    args_to_func[1], devnum, partnum);
+			for ( j = devnum; i==1 && j < max_dev_count; j++ )
+			{
+				dev_desc = get_dev(args_to_func[1], j);
+				if ((dev_desc == NULL) || (dev_desc->type==DEV_TYPE_UNKNOWN))
+					continue;
+				dev_count++;
+				sprintf(dev_part, "%d:%d", j, partnum);
+				args_to_func[2] = dev_part;
+				printf("\ntry to load script from %s %s\n",
+				    args_to_func[1], args_to_func[2]);
+				i = do_ext2load(cmdtb, 1, 5 , args_to_func);
+			}
+			if (dev_count == 0)
+				printf("No %s devices found\n", args_to_func[1]);
 			addr = simple_strtoul(args_to_func[3], NULL, 16);
 		}
 		/* finish step 1 */
@@ -156,35 +234,58 @@ int do_stage_boot(cmd_tbl_t * cmdtb, int flag, int argc, char * const argv[])
 				restore_env();
 		}
 		/* finish step 2 */
-		/* step 3 load linux image from ide */
-		if( i == 1 && strcmp(argv[device],"hd_img")==0 )
+		/* step 3 load linux image from storage device */
+		if( i == 1 && (strcmp(argv[device],"hd_img")==0 ||
+		    strcmp(argv[device],"mmc_img")==0 ||
+		    strcmp(argv[device],"usb_img")==0))
 		{
 			step = 3;
-			len=strlen(all_partions);
-			for(index=0;index<len && i==1 ;index++)
+			dev_count=0;
+			args_to_func[0]="ext2load";
+			args_to_func[3]=getenv("kernel_addr_r");
+			args_to_func[4]=path_to_image;
+			if (strcmp(argv[device],"hd_img") == 0)
 			{
-				for(j=0;j<3;j++,index++)
-					device_prt[j]=all_partions[index];
-				device_prt[3]='\0';
-				printf("\ntry to load image from ide %s\n", device_prt);
-				args_to_func[0]="ext2load";
-				args_to_func[1]=INTERFACE_HD;
-				args_to_func[2]=device_prt;
-				args_to_func[3]=getenv("kernel_addr_r");
-				args_to_func[4]=path_to_image;
+				args_to_func[1] = INTERFACE_HD;
+				max_dev_count = CONFIG_SYS_IDE_MAXDEVICE;
+			}
+			else if (strcmp(argv[device],"mmc_img") == 0)
+			{
+				args_to_func[1] = INTERFACE_MMC;
+				max_dev_count = MAX_MMC_DEV;
+			}
+			else
+			{
+				args_to_func[1] = INTERFACE_USB;
+				max_dev_count = USB_MAX_STOR_DEV;
+			}
+			printf("\nscanning %s devices beginning from: %d:%d\n",
+			    args_to_func[1], devnum, partnum);
+			for ( j = devnum; i==1 && j < max_dev_count; j++ )
+			{
+				dev_desc = get_dev(args_to_func[1], j);
+				if ((dev_desc == NULL) || (dev_desc->type==DEV_TYPE_UNKNOWN))
+					continue;
+				dev_count++;
+				sprintf(dev_part, "%d:%d", j, partnum);
+				args_to_func[2] = dev_part;
+				printf("\ntry to load image from %s %s\n",
+				    args_to_func[1], args_to_func[2]);
 				i = do_ext2load(cmdtb, 1, 5 , args_to_func);
-				addr = simple_strtoul(args_to_func[3], NULL, 16);
-				if( i==0 ){
-					int temp;
-					args_to_func[3]=getenv("ramdisk_addr_r");
-					args_to_func[4]=path_to_initrd;
-					temp = do_ext2load(cmdtb, 1, 5 , args_to_func);
-					if(temp == 0)
-					{
-						initrd = 1;
-						args_to_func[1] = getenv("kernel_addr_r");
-						args_to_func[2] = getenv("ramdisk_addr_r");
-					}
+			}
+			if (dev_count == 0)
+				printf("No %s devices found\n", args_to_func[1]);
+			addr = simple_strtoul(args_to_func[3], NULL, 16);
+			if( i==0 ){
+				int temp;
+				args_to_func[3]=getenv("ramdisk_addr_r");
+				args_to_func[4]=path_to_initrd;
+				temp = do_ext2load(cmdtb, 1, 5 , args_to_func);
+				if(temp == 0)
+				{
+					initrd = 1;
+					args_to_func[1] = getenv("kernel_addr_r");
+					args_to_func[2] = getenv("ramdisk_addr_r");
 				}
 			}
 		}
@@ -216,6 +317,7 @@ int do_stage_boot(cmd_tbl_t * cmdtb, int flag, int argc, char * const argv[])
 	}
 	free(path_to_image);
 	free(path_to_scr);
+	free(path_to_initrd);
 	path_to_image = getenv("bootargs_dflt");
 	if(!path_to_image) {
 		printf("missing environment variable: bootargs_dflt\n");
@@ -243,7 +345,13 @@ int do_stage_boot(cmd_tbl_t * cmdtb, int flag, int argc, char * const argv[])
 	#endif		
 	if(i==0)
 	{	
-		if(step == 1 || step == 4) 
+		if (step == 1)
+		{
+			sprintf(boot_source_device, "%s %s", args_to_func[1], args_to_func[2]);
+			setenv("boot_source_device", boot_source_device);
+			source(addr,NULL);
+		}
+		else if (step == 4)
 			source(addr,NULL);
 		else if ((step == 3 && initrd ==0 )|| step == 5)
 			do_bootm(cmdtb, 1,1,(char * const*)kernel_addr);
@@ -261,14 +369,18 @@ int do_stage_boot(cmd_tbl_t * cmdtb, int flag, int argc, char * const argv[])
 
 
 }
-U_BOOT_CMD(stage_boot, 6, 1,do_stage_boot, "command to load script/image from different devices\n\
+U_BOOT_CMD(stage_boot, 10, 1,do_stage_boot, "command to load script/image from different devices\n\
 	example: stage_boot hd_img pxe",
 	"Usage:\n \
-	 stage_boot hd_img - load and boot image from installed system.\n \
-	 stage_boot hd_scr - load and run script from installed system.\n \
+	 stage_boot hd_img - load and boot image from system installed on ide.\n \
+	 stage_boot hd_scr - load and run script from system installed on ide.\n \
 	 stage_boot pxe - boot from network (PXE-boot).\n \
 	 stage_boot net_img - load and boot image from tftp server.\n \
 	 stage_boot net_scr - load and run script from tftp server.\n \
+	 stage_boot usb_img - load and boot image from system installed on usb.\n \
+	 stage_boot usb_scr - load and run script from system installed on usb.\n \
+	 stage_boot mmc_img - load and boot image from system installed on mmc.\n \
+	 stage_boot mmc_scr - load and run script from system installed on mmc.\n \
 ");
 
 void save_env (void)

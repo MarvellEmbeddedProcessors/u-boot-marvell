de17bcca10d1510c01871d377e7174e5e5be9af9
Moti Buskila <motib@marvell.com>
2013-08-15 11:16:05 +0300

    ddr3: Update training libraries
    	- Update training libraries of AXP, A370, ALP and A375
    	- Reset subversion (5.2.0)
    
    Change-Id: Iea7286c610419ce74810aa7690ca8a4aae4c6a46
    Signed-off-by: Moti Buskila <motib@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/3043
    Reviewed-by: Haim Boot <hayim@marvell.com>
    Reviewed-by: Nadav Haklai <nadavh@marvell.com>
    Tested-by: Nadav Haklai <nadavh@marvell.com>

--- tools/marvell/bin_hdr/src_ddr/ddr3_init.c
+++ tools/marvell/bin_hdr/src_ddr/ddr3_init.c
@@ -452,8 +452,8 @@ MV_U32 ddr3Init_(void)
 #endif
 
 	ddr3PrintVersion();
-	DEBUG_INIT_S("1\n");
-	/* Lib version 5.1.1 */
+	DEBUG_INIT_S("0\n");
+	/* Lib version 5.2.0 */
 
 	uiFabOpt = ddr3GetFabOpt();
 	if (bPLLWAPatch){

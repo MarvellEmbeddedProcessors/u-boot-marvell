31ed77ef30b402c265d83b5395e24536c515ef92
Neta Zur <neta@marvell.com>
2013-08-15 09:07:29 +0300

    fix: common: update copyright header
    	use include to mvCopyright.h file instead of an old copyright header
    
    Change-Id: I0e25072e7e163c750bbfecd50958a194c78e6c68
    Signed-off-by: Neta Zur <neta@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/3035
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Nadav Haklai <nadavh@marvell.com>

--- board/mv_ebu/common/common/mvStack.h
+++ board/mv_ebu/common/common/mvStack.h
@@ -1,17 +1,6 @@
-/*******************************************************************************
-*                   Copyright 2003, Marvell Semiconductor Israel LTD.          *
-* THIS CODE CONTAINS CONFIDENTIAL INFORMATION OF MARVELL.                      *
-* NO RIGHTS ARE GRANTED HEREIN UNDER ANY PATENT, MASK WORK RIGHT OR COPYRIGHT  *
-* OF MARVELL OR ANY THIRD PARTY. MARVELL RESERVES THE RIGHT AT ITS SOLE        *
-* DISCRETION TO REQUEST THAT THIS CODE BE IMMEDIATELY RETURNED TO MARVELL.     *
-* THIS CODE IS PROVIDED "AS IS". MARVELL MAKES NO WARRANTIES, EXPRESSED,       *
-* IMPLIED OR OTHERWISE, REGARDING ITS ACCURACY, COMPLETENESS OR PERFORMANCE.   *
-*                                                                              *
-* MARVELL COMPRISES MARVELL TECHNOLOGY GROUP LTD. (MTGL) AND ITS SUBSIDIARIES, *
-* MARVELL INTERNATIONAL LTD. (MIL), MARVELL TECHNOLOGY, INC. (MTI), MARVELL    *
-* SEMICONDUCTOR, INC. (MSI), MARVELL ASIA PTE LTD. (MAPL), MARVELL JAPAN K.K.  *
-* (MJKK), MARVELL SEMICONDUCTOR ISRAEL LTD (MSIL).                             *
-********************************************************************************
+#include "mvCopyright.h"
+
+/********************************************************************************
 * mvStack.h - Header File for :
 *
 * FILENAME:    $Workfile: mvStack.h $

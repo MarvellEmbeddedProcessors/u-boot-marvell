19bdb6f81c29d07a8248e26466d0226182d06b45
Eli Nidam <elini@marvell.com>
2013-12-17 10:41:24 +0200

    Bump for U-Boot 2013.01-a38x-eng-drop-v7
    Change-Id: Ia368d242fe6eb5b430a585c662f6099d950b2d82
    Signed-off-by: Eli Nidam <elini@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/4754
    Tested-by: Star_Automation <star@marvell.com>

--- include/configs/armada_38x.h
+++ include/configs/armada_38x.h
@@ -51,7 +51,7 @@
 /*
  * Version
  */
-#define CONFIG_IDENT_STRING	" Marvell version: 2014_T1.0_eng_drop_v6"
+#define CONFIG_IDENT_STRING	" Marvell version: 2014_T1.0_eng_drop_v7"
 
 /* Version number passed to kernel */
 #define VER_NUM 0x13010000              /* 2013.01 */

e3f416f87e4fc11d41b28161a6f348b95197f90b
Eli Nidam <elini@marvell.com>
2014-01-23 09:22:58 +0200

    ahci: add defines for PORT_SCR_STAT register bits
    Replace hard-coded register values with proper defines for PORT_SCR_STAT
    register.
    
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>

--- drivers/block/ahci.c
+++ drivers/block/ahci.c
@@ -224,7 +224,8 @@ static int ahci_host_init(struct ahci_probe_ent *probe_ent)
 		j = 0;
 		while (j < WAIT_MS_LINKUP) {
 			tmp = readl(port_mmio + PORT_SCR_STAT);
-			if ((tmp & 0xf) == 0x3)
+			tmp &= PORT_SCR_STAT_DET_MASK;
+			if (tmp == PORT_SCR_STAT_DET_PHYRDY)
 				break;
 			udelay(1000);
 			j++;
@@ -275,7 +276,7 @@ static int ahci_host_init(struct ahci_probe_ent *probe_ent)
 		/* register linkup ports */
 		tmp = readl(port_mmio + PORT_SCR_STAT);
 		debug("SATA port %d status: 0x%x\n", i, tmp);
-		if ((tmp & 0xf) == 0x03)
+		if ((tmp & PORT_SCR_STAT_DET_MASK) == PORT_SCR_STAT_DET_PHYRDY)
 			probe_ent->link_port_map |= (0x01 << i);
 	}
 

a6bf59bca1ea2c39de4a94e9b6da019b4a58d1f5
kostap <kostap@marvell.com>
2014-01-27 18:44:51 +0200

    bc2: hal: pex: Add PCIe to DRAM access WA for BC2 (Jira - BOBCAT2-348)
    	Modify the default attribute in PCIe to DRAM window as a WA
    	for BC2 platform (shared transaction instead of default
    	non-shared one).
    	Jira reference: BOBCAT2-348
    
    Change-Id: I98444adf51bbd013b57e8d4b1ec4eb609b6d00af
    Signed-off-by: kostap <kostap@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/5318
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Yehuda Yitschak <yehuday@marvell.com>

--- board/mv_ebu/common/mv_hal/pex/mvPexAddrDec.c
+++ board/mv_ebu/common/mv_hal/pex/mvPexAddrDec.c
@@ -597,8 +597,13 @@ MV_STATUS mvPexTargetWinEnable(MV_U32 pexIf, MV_U32 winNum, MV_BOOL enable)
 
 		/* this is not relevant to default and expantion rom windows */
 		if (winRegInfo.sizeRegOffs) {
-			if ((MV_PEX_WIN_DEFAULT != winNum) && (MV_PEX_WIN_EXP_ROM != winNum))
+			if ((MV_PEX_WIN_DEFAULT != winNum) && (MV_PEX_WIN_EXP_ROM != winNum)) {
 				MV_REG_BIT_SET(winRegInfo.sizeRegOffs, PXWCR_WIN_EN);
+#ifdef MV_BOBCAT2
+				/* TODO: remove this WA once PCi to DRAM access fixed - JIRA BOBCAT2-348 */
+				MV_REG_BIT_SET(winRegInfo.sizeRegOffs, 0x1UL << (PXWCR_ATTRIB_OFFS + 4));
+#endif
+			}
 		}
 	}
 

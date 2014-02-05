c802fc39e98f40e78e5107d7e9e98085e53413da
kostap <kostap@marvell.com>
2014-02-05 11:26:57 +0200

    tools: Support automatic generation of linker scripts for binary header
    	Modify "setparams.c" utility for automatic linker script generation
    	according to board type
    
    	Add versioning information and board type parameter to setparams.c
    
    	Modify Makefile for supporting both secure and non-secure boot
    	mode builds using automatically generated linker scripts
    
    Change-Id: Ia4422565a0c5488e43f3cd4d9e85e8c26611b882
    Signed-off-by: kostap <kostap@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/5458
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Nadav Haklai <nadavh@marvell.com>

--- tools/marvell/bin_hdr/src_util/setparams.c
+++ tools/marvell/bin_hdr/src_util/setparams.c
@@ -68,23 +68,100 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include "../inc/soc_spec.h"
-#include "../inc/bootstrap_def.h"
+#include "config_marvell.h"
+#include "soc_spec.h"
+#include "bootstrap_def.h"
+
+#ifdef DEBUG
+#define DB(x...)	printf(x)
+#else
+#define DB(x...)
+#endif
+
+/* Utility version */
+#define VH	1
+#define VL	0
 
 #define  BUFSIZE	80
 
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+#endif
+
+typedef struct _boardDef {
+	char 	*bdName;
+	char 	*memory;
+	char 	*sections;
+	MV_U32	memLen;
+
+} boardDef;
+
+static boardDef boards[] = {
+	{ "gen",
+		0,
+			"\t. = %#08x;\n"
+			"\t.text : { *(.text) }\n"
+			"\t.data : { *(.data) }\n",
+	},
+	{ "a375",	/* Used also for board=alp */
+		"\tram1 : ORIGIN = %#08x, LENGTH = %#x\n"
+		"\tram2 : ORIGIN = 0x40008500, LENGTH = 0x47a0\n"
+		"\tram3 : ORIGIN = 0x4000Cca0, LENGTH = 0x3780\n"
+		"\tram4 : ORIGIN = 0x40010500, LENGTH = 0x8ac0\n",
+			"\t. = %#08x;\n"
+			"\t.text1 : {\n"
+			"\t\tbin_entry.o (.text)\n"
+			"\t\tddr3_hw_training*.o (.text)\n"
+			"\t\tddr3_init*.o (.text)\n"
+			"\t\tddr3_pbs*.o (.text)\n"
+			"\t} > ram1 = 0x0\n\n"
+			"\t.text3 : {\n"
+			"\t\tmvHighSpeedEnvSpec*.o (.text)\n"
+			"\t\tmv*.o (.text)\n"
+			"\t} > ram3 = 0x0\n\n"
+			"\t.text2 : {\n"
+			"\t\t*.o (.text)\n"
+			"\t} > ram4 = 0x0\n\n"
+			"\t.data : {\n"
+			"\t\t*(.data)\n"
+			"\t} > ram2 = 0x0\n\n"
+			"\t.bss : {\n"
+			"\t\t*(.bss)\n"
+			"\t} > ram2 = 0x0\n",
+		0x8500
+	},
+	{ "a38x",
+		"\tram1 : ORIGIN = %#08x, LENGTH = %#x\n"
+		"\tram2 : ORIGIN = 0x40020000, LENGTH = 0x8000\n",
+			"\t. = %#08x;\n"
+			"\t.text1 : {\n"
+			"\t\t*(.text)\n"
+			"\t} > ram1 = 0x0\n\n"
+			"\t.data : {\n"
+			"\t\t*(.data)\n"
+			"\t} > ram1 = 0x0\n\n"
+			"\t.bss : {\n"
+			"\t\t*(.bss)\n"
+			"\t} > ram2 = 0x0\n",
+		0x20000
+	},
+};
+
 void printusage(char *name)
 {
-	printf("Utility for BIN header preparation (parameters and linker script):\n");
+	printf("Marvell utility for BIN header preparation (parameters and linker script). Version %d.%d:\n", VH, VL);
 	printf("%s [options] <parameters output file name> <linker script output file name>\n", name);
 	printf("Options:\n");
-	printf("-S		Secure mode (secure header is included in boot image)\n");
+	printf("-S		Secure mode (secure header is included in the boot image)\n");
 	printf("-R <reg file>	REG header is present and produced from \"reg file\" content\n");
 	printf("-P <par file>	Text file with BIN header parameters. The text file contains\n");
-	printf("-O <Hex value>	Calculated offset of the bin header\n");
 	printf("		one parametes per line, up to 256 lines total. Each parameter is\n");
 	printf("		a hexadecimal number in range of 0 to 2^32.\n");
-
+	printf("-B <board_type>	The target board type. Supported bards: \n");
+	printf("-		gen - generic board, used also if the requested board is not known\n");
+	printf("-		alp  - Avanta LP.\n");
+	printf("-		a375 - Armada 375.\n");
+	printf("-		a38x - Armada 38x family.\n");
 }
 
 int main(int argc, char **argv)
@@ -98,11 +175,12 @@ int main(int argc, char **argv)
 	unsigned long	memalign;
 	FILE		*f_outparm = NULL, *f_outlink = NULL;
 	FILE		*f_reg = NULL, *f_param = NULL;
+	char		*board_name = NULL;
 	int 		optch; /* command-line option char */
 	static char	optstring[] = "SR:P:B:";
-	struct stat st;
-	
-	offset  = RAM_TOP + HDR_BLK_OFFSET;
+	int		bIdx;
+
+	offset  = HDR_BLK_OFFSET;
 
 	while ((optch = getopt(argc, argv, optstring)) != -1) {
 		switch (optch) {
@@ -126,9 +204,8 @@ int main(int argc, char **argv)
 			}
 			break;
 
-		case 'B': /* Parameters file */
-			stat(optarg, &st);
-			offset += st.st_size + 8; /* bytes (4 - header type and length + 4- Next header and 3 reserved bytes) */
+		case 'B': /* Board type */
+			board_name = optarg;
 			break;
 
 
@@ -136,6 +213,8 @@ int main(int argc, char **argv)
 			goto parse_error;
 		}
 	}
+
+	printf("Building linker script for board %s, %s mode\n", board_name, secure == 1 ? "Secure" : "Non-secure");
 
 	f_outparm = fopen(argv[optind], "w");
 	if (f_outparm == NULL) {
@@ -174,18 +253,18 @@ int main(int argc, char **argv)
 			argsnum++;
 		}
 	}
-	printf("Supplied %ld parameters in TXT file\n", argsnum);
+	DB("Supplied %ld parameters in TXT file\n", argsnum);
 
 	/* main and secure headers */
 	offset += sizeof(BHR_t) + secure * sizeof(secExtBHR_t);
 	/* register header */
 	if (reghdr_argsnum != 0) {
 		offset += EXT_HDR_BASE_SIZE + reghdr_argsnum * 8;
-		printf("The REG configuration consists %ld (ADDR,VAL) pairs\n", reghdr_argsnum);
+		DB("The REG configuration consists %ld (ADDR,VAL) pairs\n", reghdr_argsnum);
 	}
-	/* binary header - one additional word is for number of arguments */
+	/* binary header - one additional word is for number of arguments field */
 	offset += sizeof(headExtBHR_t) + 4 + argsnum * 4;
-	/* printf("SUSUSUSU %x\n\n\n",offset); */
+	DB("Offset before alignment is %#08lx \n", offset + RAM_TOP);
 
 	/* The start address of executable code should be aligned to 128bit boundary using dumb arguments
 	   Othervise the linker will silently align it and the execution will start at the wrong place */
@@ -193,21 +272,38 @@ int main(int argc, char **argv)
 	if (memalign != 0) {
 		argsnum += (0x10 - memalign) / 4;
 		offset = (offset & 0xFFFFFFF0UL) + 0x10;
-		printf("\n****WARNING: Adding %ld empty parameters to the BIN header\n\n",
-		       (0x10 - memalign) / 4);
-	}
+		DB("Code section aligned with %ld empty parameters added to BIN header\n", (0x10 - memalign) / 4);
+	} else
+		DB("No alignment is needed\n");
 
 	/* Number of arguments field always presents in BIN header */
 	fwrite(&argsnum, 4, 1, f_outparm);
 	if (argsnum != 0)
 		fwrite(args, 4, argsnum, f_outparm);
 
+	/* Seek for the board in parameters array */
+	for (bIdx = 0; bIdx < ARRAY_SIZE(boards); bIdx++) {
+		if ((strcmp(boards[bIdx].bdName, board_name) == 0) ||
+		     /* alp should use the same parameters as a375 */
+		    ((strcmp(boards[bIdx].bdName, "a375") == 0) && (strcmp(board_name, "alp") == 0)))
+			break;
+	}
+	/* Select generic linker script for unknown board */
+	if (bIdx == ARRAY_SIZE(boards)) {
+		DB("Board %s was not found in database, using generic script\n", board_name);
+		bIdx = 0;
+	}
+
 	fprintf(f_outlink, "OUTPUT_FORMAT(\"elf32-littlearm\", \"elf32-bigarm\", \"elf32-littlearm\")\n");
-	fprintf(f_outlink, "OUTPUT_ARCH(arm)\n");
+	fprintf(f_outlink, "OUTPUT_ARCH(arm)\n\n");
+	if (boards[bIdx].memory != 0) {
+		fprintf(f_outlink, "MEMORY\n{\n");
+		fprintf(f_outlink, boards[bIdx].memory, offset + RAM_TOP, boards[bIdx].memLen - offset);
+		fprintf(f_outlink, "}\n");
+	}
 	fprintf(f_outlink, "SECTIONS\n{\n");
-	fprintf(f_outlink, "\t. = %#08lX;\n", offset);
-	fprintf(f_outlink, "\t.text : { *(.text) }\n");
-	fprintf(f_outlink, "\t.data : { *(.data) }\n}\n");
+	fprintf(f_outlink, boards[bIdx].sections, offset + RAM_TOP);
+	fprintf(f_outlink, "}\n");
 
 	err = 0;
 

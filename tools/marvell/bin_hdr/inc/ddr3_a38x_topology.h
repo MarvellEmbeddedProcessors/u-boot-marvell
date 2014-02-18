274d4c42171f29c39a93786df85b1c8d001268f1
Igor Patrik <igorp@marvell.com>
2014-02-18 11:49:34 +0200

    a38x: Fix for corrupted data in NAS tests
    	This patch fixed fixing corrupted data in NAS tests.
    	This change fixing Refresh Interval Count value in 0x1400 to proper value.
    
    Change-Id: I37da669c1af757634ce5e7530979cef1e7a58bda
    Signed-off-by: Igor Patrik <igorp@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/5759
    Reviewed-by: Haim Boot <hayim@marvell.com>
    Tested-by: Star_Automation <star@marvell.com>

--- tools/marvell/bin_hdr/inc/ddr3_a38x_topology.h
+++ tools/marvell/bin_hdr/inc/ddr3_a38x_topology.h
@@ -94,7 +94,7 @@ MV_HWS_TOPOLOGY_MAP a38xTopologyMap[] =
     {
     0x1, /* active interfaces */
     /*cs_mask, mirror, dqs_swap, ck_swap X PUPs                                                                     speed_bin        memory_width  mem_size     frequency  casL casWL      temperature */
-	{{{{0x1,0,0,0}, {0x1,0,0,0}, {0x3,0,0,0}, {0x3,0,0,0}, {0x0,0,0,0}}, SPEED_BIN_DDR_1866L, ACTUAL_BUS_WIDTH , MEM_4G, DDR_FREQ_800, 0 ,   0 , MV_HWS_TEMP_HIGH}},
+	{{{{0x1,0,0,0}, {0x1,0,0,0}, {0x3,0,0,0}, {0x3,0,0,0}, {0x0,0,0,0}}, SPEED_BIN_DDR_1866L, ACTUAL_BUS_WIDTH , MEM_4G, DDR_FREQ_800, 0 ,   0 , MV_HWS_TEMP_LOW}},
     5, /* Num Of Bus Per Interface*/
     ACTUAL_BUS_MASK,  /* Buses mask */
     0  /* board number */
@@ -103,7 +103,7 @@ MV_HWS_TOPOLOGY_MAP a38xTopologyMap[] =
     {
     0x1, /* active interfaces */
     /*cs_mask, mirror, dqs_swap, ck_swap X PUPs                                     speed_bin             memory_width  mem_size     frequency  casL casWL      temperature */
-	{{{{0x1,0,0,0},{ 0x1,0,0,0},{ 0x2,1,0,0},{ 0x2,1,0,0}, {0,0,0,0}}, SPEED_BIN_DDR_1866L, ACTUAL_BUS_WIDTH , MEM_4G, DDR_FREQ_800, 0 ,   0 , MV_HWS_TEMP_HIGH}},
+	{{{{0x1,0,0,0},{ 0x1,0,0,0},{ 0x2,1,0,0},{ 0x2,1,0,0}, {0,0,0,0}}, SPEED_BIN_DDR_1866L, ACTUAL_BUS_WIDTH , MEM_4G, DDR_FREQ_800, 0 ,   0 , MV_HWS_TEMP_LOW}},
     5, /* Num Of Bus Per Interface*/
     ACTUAL_BUS_MASK,  /* Buses mask */
     1  /* board number */

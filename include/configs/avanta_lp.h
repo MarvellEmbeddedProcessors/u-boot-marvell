66de65725de3e832c915f9933da70d213491e041
kostap <kostap@marvell.com>
2014-03-05 18:13:30 +0200

    fix: nand: alp: Enlarge the u-boot flash allocation size
            Increase the value of CONFIG_UBOOT_SIZE in alp
            configuration file for accepting larger images
    
    Change-Id: I76d42b9b91aaa3f9fa8dc9bac6c6b878330025f4
    Signed-off-by: kostap <kostap@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/6158
    Reviewed-by: Omri Itach <omrii@marvell.com>
    Tested-by: Omri Itach <omrii@marvell.com>

--- include/configs/avanta_lp.h
+++ include/configs/avanta_lp.h
@@ -509,7 +509,7 @@ extern unsigned int mvTclkGet(void);
 #define PCI_HOST_AUTO    2              /* detected via arbiter enable  */
 
 
-#define CONFIG_UBOOT_SIZE                       0xE0000
+#define CONFIG_UBOOT_SIZE                       0x100000
 #define CONFIG_SPARE_AREA			0x400000
 /*
  * NOR Flash

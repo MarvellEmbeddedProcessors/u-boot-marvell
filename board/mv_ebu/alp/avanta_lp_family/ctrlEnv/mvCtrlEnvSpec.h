1649230180fbd2dc8fe1ba65c1c9f53ac6d6e62a
Yehuda Yitschak <yehuday@marvell.com>
2014-03-12 17:11:08 +0200

    dragonite: alp: Add driver to initialize dragonite coprocessor
    Change-Id: I8bf6afbc8cc25f75477bfa55b47f1a14d8c0ff98
    Signed-off-by: Yehuda Yitschak <yehuday@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/6377
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Shadi Ammouri <shadi@marvell.com>

--- board/mv_ebu/alp/avanta_lp_family/ctrlEnv/mvCtrlEnvSpec.h
+++ board/mv_ebu/alp/avanta_lp_family/ctrlEnv/mvCtrlEnvSpec.h
@@ -487,6 +487,7 @@ typedef enum _mvTarget {
 	PP2_CPU0,	/* 26 PP2 - CPU 0		*/
 	PP2_CPU1,	/* 27 PP2 - CPU 1		*/
 	DFEV,		/* 28 DFEV Unit			*/
+	DRAGONITE,	/* 29 Dragonite co-processor	*/
 	MAX_TARGETS
 } MV_TARGET;
 
@@ -540,6 +541,7 @@ typedef enum _mvTarget {
 	{ 0x00, PP2_TARGET_ID	},		/* PP2 - CPU 0           */ \
 	{ 0x01, PP2_TARGET_ID	},		/* PP2 - CPU 1           */ \
 	{ 0x00, DFEV_TARGET_ID },		/* DFEV			 */ \
+	{ 0x14, DRAGONITE_TARGET_ID },		/* DRAGONITE		 */ \
 }
 
 #define CESA_TARGET_NAME_DEF	("CRYPT_ENG0", "CRYPT_ENG1")
@@ -572,7 +574,8 @@ typedef enum _mvTarget {
 	"CRYPT1_ENG",		/* CRYPT1_ENG */	\
 	"PP2 - CPU 0",		/* PP2 - CPU 0 */	\
 	"PP2 - CPU 1",		/* PP2 - CPU 1 */	\
-	"DFEV"			/* DFEV */		\
+	"DFEV",			/* DFEV */		\
+	"DRAGONITE"		/* DRAGONITE */		\
 }
 
 

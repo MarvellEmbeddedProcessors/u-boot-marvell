fae3be798aba8163ad2092f3ccb7724eacff5387
repo <omrii@marvell.com>
2014-05-08 11:53:22 +0300

    Bump U-Boot-2013.01 to release 2014_T2.0_eng_dropv7
    Change-Id: I177b28f082ca52b236c0a971f5e036aa2ebcc852
    Signed-off-by: repo <omrii@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/7883
    Reviewed-by: Eli Nidam <elini@marvell.com>
    Tested-by: Star_Automation <star@marvell.com>

--- include/configs/avanta_lp.h
+++ include/configs/avanta_lp.h
@@ -55,7 +55,7 @@
 /*
  * Version
  */
-#define CONFIG_IDENT_STRING	" Marvell version: 2014_T2.0_eng_dropv6"
+#define CONFIG_IDENT_STRING	" Marvell version: 2014_T2.0_eng_dropv7"
 
 /* Version number passed to kernel */
 #define VER_NUM 0x11120000              /* 2011.12 */

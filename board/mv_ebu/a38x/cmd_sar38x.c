5e4b80cf2606f71f743c4aa52bd6e765e84bee80
Omri Itach <omrii@marvell.com>
2014-07-21 12:18:04 +0300

    fix: sample at reset: a38x: added 'sgmiispeed' default 'SatR' value
    	now 'SatR write default' restores 'sgmiispeed' fields to be 1G as default for all boards
    
    Change-Id: Ief359a40606e2196388ea4bf20644aa2f53abfd5
    Signed-off-by: Omri Itach <omrii@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/9521
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Hanna Hawa <hannah@marvell.com>

--- board/mv_ebu/a38x/cmd_sar38x.c
+++ board/mv_ebu/a38x/cmd_sar38x.c
@@ -45,13 +45,14 @@ typedef struct _boardSatrDefault {
 	MV_SATR_TYPE_ID satrId;
 	MV_U32 defauleValueForBoard[MV_MARVELL_BOARD_NUM];
 } MV_BOARD_SATR_DEFAULT;
-#define MAX_DEFAULT_ENTRY	4
+#define MAX_DEFAULT_ENTRY	5
 MV_BOARD_SATR_DEFAULT boardSatrDefault[MAX_DEFAULT_ENTRY] = {
 /* 	defauleValueForBoard[] = RD_NAS_68xx,	DB_BP_68xx,	RD_WAP_68xx,	DB_AP_68xx */
 { MV_SATR_CPU_DDR_L2_FREQ,	{0x0c,		0x0c,		0x0c,		0x0c}},
 { MV_SATR_CORE_CLK_SELECT,	{1,		1,		1,		1}},
 { MV_SATR_CPU1_ENABLE,	  	{MV_TRUE,	MV_TRUE,	MV_TRUE,	MV_TRUE}},
 { MV_SATR_SSCG_DISABLE,	  	{MV_FALSE,	MV_FALSE,	MV_FALSE,	MV_FALSE}},
+{ MV_SATR_SGMII_SPEED,		{0,		0,		0,		0}},
 };
 int do_sar_default(void)
 {

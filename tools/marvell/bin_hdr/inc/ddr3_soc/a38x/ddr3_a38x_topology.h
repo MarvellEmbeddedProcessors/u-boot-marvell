b31b6ad4f605431dbfe38ce007a46a8b7b1cea4a
Igor Patrik <igorp@marvell.com>
2014-07-24 12:52:22 +0300

    fix: ddr: a38x: set DRAM bus width as 32 for DB-AP
    	The board support 32bit mode
    
    Change-Id: I48bc164682260b14869aebcf6364056553884141
    Signed-off-by: Igor Patrik <igorp@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/9605
    Reviewed-by: Omri Itach <omrii@marvell.com>
    Tested-by: Omri Itach <omrii@marvell.com>

--- tools/marvell/bin_hdr/inc/ddr3_soc/a38x/ddr3_a38x_topology.h
+++ tools/marvell/bin_hdr/inc/ddr3_soc/a38x/ddr3_a38x_topology.h
@@ -149,7 +149,7 @@ MV_HWS_TOPOLOGY_MAP TopologyMap[] =
     /*cs_mask, mirror, dqs_swap, ck_swap X PUPs                                     speed_bin             memory_width  mem_size     frequency  casL casWL      temperature */
 	{{{{0x1,0,0,0},{ 0x1,0,0,0},{ 0x2,1,0,0},{ 0x2,1,0,0}, {0,0,0,0}}, SPEED_BIN_DDR_1866L, ACTUAL_BUS_WIDTH , MEM_4G, DDR_FREQ_800, 0 ,   0 , MV_HWS_TEMP_LOW}},
     5, /* Num Of Bus Per Interface*/
-    BUS_MASK_16BIT  /* Buses mask */
+    ACTUAL_BUS_MASK  /* Buses mask */
     }
 };
 #endif /* CONFIG_CUSTOMER_BOARD_SUPPORT */

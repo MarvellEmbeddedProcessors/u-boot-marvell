b92275546ec7a6e17d2227ab02ef93390e68a8ee
Tal Cohen <talc@marvell.com>
2014-08-14 15:09:20 +0300

    spi-nand: minor - code intention
    Change-Id: I8a9cc5677cef72d6b48d870771c4d539f5744320
    Signed-off-by: Tal Cohen <talc@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/10556
    Reviewed-by: Omri Itach <omrii@marvell.com>
    Tested-by: Star_Automation <star@marvell.com>

--- board/mv_ebu/common/USP/nand_spi.c
+++ board/mv_ebu/common/USP/nand_spi.c
@@ -69,32 +69,32 @@ int board_nand_init(struct nand_chip *nand)
 
 static void nand_spi_init(struct nand_chip *nand, struct nand_spi *nspi)
 {
-	nand->num_devs		= 1;
-	nand->waitfunc		= nand_spi_waitfunc;
-	nand->dev_ready		= 0;
+	nand->num_devs       = 1;
+	nand->waitfunc       = nand_spi_waitfunc;
+	nand->dev_ready      = 0;
 
-	nand->cmdfunc		= nand_spi_cmdfunc;
+	nand->cmdfunc       = nand_spi_cmdfunc;
 	nand->select_chip   = nand_spi_select_chip;
-	nand->read_byte		= nand_spi_read_byte;
+	nand->read_byte     = nand_spi_read_byte;
 
-	nand->read_word		= nand_spi_read_word;
-	nand->read_buf		= nand_spi_read_buf;
-	nand->write_buf		= nand_spi_write_buf;
-	nand->verify_buf	= 0;
-	nand->block_markbad	= 0;
+	nand->read_word     = nand_spi_read_word;
+	nand->read_buf      = nand_spi_read_buf;
+	nand->write_buf     = nand_spi_write_buf;
+	nand->verify_buf    = 0;
+	nand->block_markbad = 0;
 
-	nand->ecc.mode		= NAND_ECC_HW;
-	nand->ecc.hwctl		= nand_spi_ecc_hwctl;
-	nand->ecc.calculate	= nand_spi_ecc_calculate;
-	nand->ecc.correct	= nand_spi_ecc_correct;
+	nand->ecc.mode      = NAND_ECC_HW;
+	nand->ecc.hwctl     = nand_spi_ecc_hwctl;
+	nand->ecc.calculate = nand_spi_ecc_calculate;
+	nand->ecc.correct   = nand_spi_ecc_correct;
 
 /* Hardware generates ECC per 512 Bytes, 4 Bytes per step */
-	nand->ecc.size		= 512;
+	nand->ecc.size      = 512;
 	nand->ecc.bytes     = 4;
-	nand->ecc.layout	= &nand_spi_ecc;
+	nand->ecc.layout    = &nand_spi_ecc;
 
-	nand->bbt_td 		   = 0;
-	nand->bbt_md 		   = 0;
+	nand->bbt_td           = 0;
+	nand->bbt_md           = 0;
 	nand->badblock_pattern = &nand_spi_bbt;
 
 	nspi->page_size       = NAND_SPI_PAGE_SIZE;
@@ -155,7 +155,7 @@ static int nand_spi_wait_ready(struct nand_spi *nspi, unsigned long timeout)
 	do {
 		spi_flash_cmd_read(nspi->spi, cmd, 2, buf, 1);
 		debug_spi(" = 0x%x", buf[0]);
-		
+
 		if ((buf[0] & SR_OIP_BIT) == 0)
 			break;
 
@@ -176,7 +176,7 @@ static int nand_spi_wait_ready(struct nand_spi *nspi, unsigned long timeout)
 static void nand_spi_cmdfunc(struct mtd_info *mtd, unsigned command, int column, int page_addr)
 {
 	struct nand_spi *nspi = (struct nand_spi *)((struct nand_chip *)mtd->priv)->priv;
-	
+
 	debug_spi("nand-spi:command=0x%x(%d) column=%d page_addr=%d\n", command, command, column, page_addr);
 	
 	switch (command) {
@@ -231,7 +231,7 @@ static int nand_spi_waitfunc(struct mtd_info *mtd, struct nand_chip *nspi)
 static void nand_spi_select_chip(struct mtd_info *mtd, int chip)
 {
 	int ret;
-	struct nand_spi *nspi  = (struct nand_spi *)((struct nand_chip *)mtd->priv)->priv;
+	struct nand_spi *nspi = (struct nand_spi *)((struct nand_chip *)mtd->priv)->priv;
 
 	/*spi_cs_activate(nspi->spi);*/
 
@@ -251,7 +251,7 @@ static int nand_spi_ecc_correct(struct mtd_info *mtd, uint8_t *dat, uint8_t *rea
 	 * 11b = bit error was detected and corrected, error bit number = 8
 	 */
 
-	struct nand_spi *nspi = (struct nand_spi *)((struct nand_chip *)mtd->priv)->priv;	
+	struct nand_spi *nspi = (struct nand_spi *)((struct nand_chip *)mtd->priv)->priv;
 	if(nspi->ecc_status == ECC_ERR_NOT_CORRECTED)
 		return -1;
 	
@@ -265,7 +265,7 @@ static int nand_spi_enable(struct nand_spi *nspi)
 
 	/* Write Enable */
 	ret = spi_flash_cmd(nspi->spi, CMD_WREN, NULL, 0);
-	if (ret < 0) {
+	if(ret < 0) {
 		debug_spi("nand-spi:enabling write failed\n");
 		return ret;
 	}
@@ -275,7 +275,7 @@ static int nand_spi_enable(struct nand_spi *nspi)
 	cmd[1] = 0xA0;
 	cmd[2] = 0x0;
 	ret = spi_flash_cmd_write(nspi->spi, cmd, 3, NULL, 0);
-	if (ret < 0) {
+	if(ret < 0) {
 		debug_spi("nand-spi:write status register failed\n");
 		return ret;
 	}
@@ -327,7 +327,7 @@ static void nand_spi_cmd_status(struct nand_spi *nspi)
 	spi_flash_cmd_read(nspi->spi, cmd, 2, buf, 1);
 	debug_spi("nand-spi:get features:\nprotection register (command 0x%x, %x), value=0x%x\n", 
 			  CMD_RDSR, 0xA0, buf[0]);
-	
+
 	/* Get Features : feature register */
 	cmd[0] = CMD_RDSR;
 	cmd[1] = 0xB0; /* Protection status */
@@ -340,8 +340,8 @@ static void nand_spi_cmd_status(struct nand_spi *nspi)
 	spi_flash_cmd_read(nspi->spi, cmd, 2, buf, 3);
 	debug_spi("status register (command 0x%x, %x), value=0x%x\n", CMD_RDSR, 0xC0, buf[0]);
 
-	/* The nand stack check if the device is in write protected state by test Bit-7 (0x80)
-	   while NAND-SPI use Bit-1 (0x02) to indicates write protected state					*/
+	/* The nand stack check if the device is in write protected state by test Bit-7 (0x80) */
+	/*   while NAND-SPI use Bit-1 (0x02) to indicates write protected state                */
 	if(buf[0] & WP_BIT) {
 		buf[0] |= 0x80;
 	}
@@ -382,7 +382,7 @@ static void nand_spi_cmd_write(struct nand_spi *nspi)
 
 	/* Write Enable */
 	ret = spi_flash_cmd(nspi->spi, CMD_WREN, NULL, 0);
-	if (ret < 0) {
+	if(ret < 0) {
 		debug_spi("nand-spi:enabling Write failed\n");
 		return;
 	}
@@ -393,15 +393,15 @@ static void nand_spi_cmd_write(struct nand_spi *nspi)
 		debug_spi("nand-spi:page programming timed out\n");
 	}
 
-	/* Program Execute: Write page from Cache to flash
-	 * command requires a 24-bit address consisting of:
-	 * - 8 dummy bits
-	 * - 10 bits of sector number
-	 * - 6 bit of page number */
+	/* Program Execute: Write page from Cache to flash    */
+	/* command requires a 24-bit address consisting of:   */
+	/* - 8 dummy bits                                     */
+	/* - 10 bits of sector number                         */
+	/* - 6 bit of page number                             */
 	cmd[0]  = CMD_PROGRAM_EXECUTE;
 	cmd[1]  = 0;				        /* Dummy */
-	cmd[2]  = sector_num >> 2;		    /* 8 bits sector number (8 MSB bits) */
-	cmd[3]  = page_num & 0x3f;		    /* 6 LSB bits of cmd[0] = page address */
+	cmd[2]  = sector_num >> 2;		    /* 8 bits sector number (8 MSB bits)                  */
+	cmd[3]  = page_num & 0x3f;		    /* 6 LSB bits of cmd[0] = page address                */
 	cmd[3] |= (sector_num & 0x3) << 6;	/* 2 LSB bits of cmd[0]= 1st 2 bits of sector address */
 
 	ret = spi_flash_cmd_write(nspi->spi, cmd, 4, NULL, 0);
@@ -436,11 +436,11 @@ static void nand_spi_cmd_read(struct nand_spi *nspi)
 	/*64 pages per sector*/
 	sector_num = (page_num >> 6);
 
-	/* Read page from Flash to Cache:
-	 * command requires a 24-bit address consisting of
-	 * - 8 dummy bits
-	 * - 10 bits of sector number
-	 * - 6 bit of page number */
+	/* Read page from Flash to Cache:                  */
+	/* command requires a 24-bit address consisting of */
+	/* - 8 dummy bits                                  */
+	/* - 10 bits of sector number                      */
+	/* - 6 bit of page number                          */
 	cmd[0] = CMD_READ_TO_CACHE;
 	cmd[1] = 0;
 	cmd[2] = sector_num >> 2;
@@ -505,8 +505,8 @@ static void nand_spi_cmd_read_oob(struct nand_spi *nspi)
 	cmd[0] = CMD_READ_TO_CACHE;
 	cmd[1] = 0;
 	cmd[2] = sector_num >> 2;
-	cmd[3] = page_num & 0x3f;		    /* 6 LSB bits */
-	cmd[3] |= (sector_num & 0x3) << 6;	/* 2 MSB bits */
+	cmd[3] = page_num & 0x3f;             /* 6 LSB bits */
+	cmd[3] |= (sector_num & 0x3) << 6;    /* 2 MSB bits */
 
 	ret = spi_flash_cmd_write(nspi->spi, cmd, 4, NULL, 0);
 	if (ret < 0) {
@@ -523,7 +523,7 @@ static void nand_spi_cmd_read_oob(struct nand_spi *nspi)
 
 	/* Read oob data from Cache */
 	cmd[0]  = CMD_READ_FROM_CACHE;
-	cmd[1]  = 0x80;   /*64-len for oob area*/
+	cmd[1]  = 0x80;   /*64-len for oob area                         */
 	cmd[1] |= 0x08;   /*offset(2-bits MSB) 2048 for OOB data (0x800)*/
 	cmd[2]  = 0x0;    /*offset(4-bits-LSB) 2048 for OOB data (0x800)*/
 	cmd[3]  = 0x0;
@@ -560,8 +560,8 @@ static void nand_spi_cmd_erase(struct nand_spi *nspi)
 	 * - 8 dummy bits
 	 * - 10 bits of sector number
 	 * - 6 bit of page number */
-	cmd[2] = sector_num >> 2;		/* 6 MSB bits */
-	cmd[3] = (sector_num & 0x3) << 6;	/* 2 LSB bits */
+	cmd[2] = sector_num >> 2;              /* 6 MSB bits */
+	cmd[3] = (sector_num & 0x3) << 6;      /* 2 LSB bits */
 	debug_spi("nand-spi:erase. sector_num=%lu \n", sector_num);
 
 	ret = spi_flash_cmd(nspi->spi, CMD_WREN, NULL, 0);

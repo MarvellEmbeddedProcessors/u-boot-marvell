b64af06cd96c5c71915e78b70af02d76f05419e2
Omri Itach <omrii@marvell.com>
2014-08-25 13:20:27 +0300

    Bump U-Boot 2013.01 to release 2014_T3.0_eng_dropv6
    Change-Id: I11aa7c5dea8de87d0605e8d1307e78b1c5925302
    Signed-off-by: Omri Itach <omrii@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/11278
    Reviewed-by: Kostya Porotchkin <kostap@marvell.com>
    Tested-by: Star_Automation <star@marvell.com>

--- include/configs/armada_375.h
+++ include/configs/armada_375.h
@@ -51,7 +51,7 @@
 /*
  * Version
  */
-#define CONFIG_IDENT_STRING	" Marvell version: 2014_T3.0_eng_dropv5"
+#define CONFIG_IDENT_STRING	" Marvell version: 2014_T3.0_eng_dropv6"
 
 /* Version number passed to kernel */
 #define VER_NUM 0x13010000              /* 2013.01 */

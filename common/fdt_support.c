a00f527dadf7a8e9e70f5a44f7801f1c8917c2c9
Omri Itach <omrii@marvell.com>
2014-09-08 15:52:49 +0300

    fix: fdt: added mtu update sequence to ethernet's node update
    	- Added MTU property to fdt automatic ethernet update sequence.
    	- MTU values are taken from env variables: ethmtu, eth1mtu, eth2mtu, ...
    	- default MTU = 1500 (if ethmtu, eth1mtu, eth2mtu,.. doesn't exists).
    	* JIRA SYSTEMSW: #1007, #1048
    
    Change-Id: I1fa68883863de8f044322e5fad3305bc75d220c8
    Signed-off-by: Omri Itach <omrii@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/11605
    Tested-by: Star_Automation <star@marvell.com>

--- common/fdt_support.c
+++ common/fdt_support.c
@@ -448,8 +448,10 @@ void fdt_fixup_ethernet(void *fdt)
 	int node, i, j;
 	char enet[16], *tmp, *end;
 	char mac[16] = "ethaddr";
+	char mtu[16] = "ethmtu";
 	const char *path;
 	unsigned char mac_addr[6];
+	ulong ulongtmp;
 
 	node = fdt_path_offset(fdt, "/aliases");
 	if (node < 0)
@@ -475,7 +477,11 @@ void fdt_fixup_ethernet(void *fdt)
 		do_fixup_by_path(fdt, path, "local-mac-address",
 				&mac_addr, 6, 1);
 
+		ulongtmp = getenv_ulong(mtu, 10, 1500);
+		do_fixup_by_path_u32(fdt, path, "eth,port-mtu", (u32)ulongtmp, 0);
+
 		sprintf(mac, "eth%daddr", ++i);
+		sprintf(mtu, "eth%dmtu", i);
 	}
 }
 

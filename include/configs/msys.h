1837d4caf7c5a7ed7bc6dafd2f2d973836f980fc
Hanna Hawa <hannah@marvell.com>
2014-09-28 21:39:28 +0300

    Bump U-Boot 2013.01 to release 2014_T3.0p1
    Change-Id: I38e8b1035b5a2165e72eb568aded75178026524d
    Signed-off-by: Omri Itach <omrii@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/13771

--- include/configs/msys.h
+++ include/configs/msys.h
@@ -44,7 +44,7 @@ disclaimer.
 /************/
 /* VERSIONS */
 /************/
-#define CONFIG_IDENT_STRING	" Marvell version: 2014_T3.0"
+#define CONFIG_IDENT_STRING	" Marvell version: 2014_T3.0p1"
 
 /* version number passing when loading Kernel */
 #define VER_NUM 0x11120000           /* 2011.12 */

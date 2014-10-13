e60cada46adc369827120185dc42ce2c10493f74
Omri Itach <omrii@marvell.com>
2014-10-13 10:23:56 +0200

    fix: fdt: a38x: add fdt file name definition for DB-GP board
    Change-Id: I97b207a025c287cc616579c8763589ab00055921
    Signed-off-by: Omri Itach <omrii@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/13581
    Reviewed-by: Hanna Hawa <hannah@marvell.com>
    Tested-by: Star_Automation <star@marvell.com>

--- board/mv_ebu/a38x/mv_main_a38x.c
+++ board/mv_ebu/a38x/mv_main_a38x.c
@@ -632,7 +632,8 @@ void misc_init_r_env(void)
 #ifdef CONFIG_CUSTOMER_BOARD_SUPPORT
 		fdt_env_setup("armada-38x.dtb");
 #else
-		char *fdtfiles[] = {"armada-388-rd.dtb", "armada-385-db.dtb", "armada-385-rd.dtb", "armada-385-db-ap.dtb"};
+		char *fdtfiles[] = {"armada-388-rd.dtb", "armada-385-db.dtb", "armada-385-rd.dtb",\
+					"armada-385-db-ap.dtb", "armada-388-db-gp.dtb"};
 		fdt_env_setup(fdtfiles[mvBoardIdIndexGet(mvBoardIdGet())]);
 #endif
 

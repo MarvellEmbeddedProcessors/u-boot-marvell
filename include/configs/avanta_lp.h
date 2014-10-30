0a843ae4cab6f2b8c9739225ee4bb163eb8d95fa
Hanna Hawa <hannah@marvell.com>
2014-10-30 16:58:31 +0200

    Bump U-Boot 2013.01 to release 2015_T1.0.eng_drop_v1
    Change-Id: I1e629dc5bc1f087d9f7f5fa7f73a2d812e8ffe05
    Signed-off-by: Hanna Hawa <hannah@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/13957
    Reviewed-by: Omri Itach <omrii@marvell.com>
    Tested-by: Omri Itach <omrii@marvell.com>

--- include/configs/avanta_lp.h
+++ include/configs/avanta_lp.h
@@ -55,7 +55,7 @@
 /*
  * Version
  */
-#define CONFIG_IDENT_STRING	" Marvell version: 2014_T3.0"
+#define CONFIG_IDENT_STRING	" Marvell version: 2015_T1.0.eng_drop_v1"
 
 /* Version number passed to kernel */
 #define VER_NUM 0x11120000              /* 2011.12 */

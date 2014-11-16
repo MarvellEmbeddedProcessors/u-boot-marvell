6c8510718b6930bf1d9719936d9a70278c2f76fd
Igor Patrik <igorp@marvell.com>
2014-11-16 11:00:32 +0200

    a39x: ddr4: replace default bus width for DDR4 from 8 to 16
    	Decided that default DDR4 modules will be by16
    
    Change-Id: Icb4b548f8d93c065860175b3e5a5764eb8994a0e
    Signed-off-by: Igor Patrik <igorp@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/14375
    Reviewed-by: Omri Itach <omrii@marvell.com>
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/14401

--- tools/marvell/bin_hdr/inc/ddr3_soc/a39x/ddr3_a39x_topology.h
+++ tools/marvell/bin_hdr/inc/ddr3_soc/a39x/ddr3_a39x_topology.h
@@ -116,7 +116,7 @@ MV_HWS_TOPOLOGY_MAP TopologyMap[] =
     {
     0x1, /* active interfaces */
     /*cs_mask, mirror, dqs_swap, ck_swap X PUPs                                     speed_bin             memory_width  mem_size     frequency  casL casWL      temperature */
-    {{{{0x1,0,0,0},{ 0x1,0,0,0},{ 0x1,0,0,0},{ 0x1,0,0,0}, {0,0,0,0}}, SPEED_BIN_DDR_2133P, BUS_WIDTH_8, MEM_4G, DDR_FREQ_800, 0 ,   0 , MV_HWS_TEMP_LOW}},
+    {{{{0x1,0,0,0},{ 0x1,0,0,0},{ 0x1,0,0,0},{ 0x1,0,0,0}, {0,0,0,0}}, SPEED_BIN_DDR_2133P, BUS_WIDTH_16, MEM_4G, DDR_FREQ_800, 0 ,   0 , MV_HWS_TEMP_LOW}},
     5, /* Num Of Bus Per Interface*/
     BUS_MASK_32BIT  /* Buses mask */
     },

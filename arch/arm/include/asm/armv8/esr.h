686ac8452d30901c88c8876e3d186b9a1aaf99db
Arnab Basu <arnab_basu@rocketmail.com>
2015-01-13 02:26:45 +0530

    ARMv8: PCSI: Add generic ARMv8 PSCI code
    Implement core support for PSCI. As this is generic code, it doesn't
    implement anything really useful (all the functions are returning
    Not Implemented).
    
    This is largely ported from the similar code that exists for ARMv7
    
    Change-Id: Id4c909c6632049698de0ae0c97d47847c7ed2b3f
    Signed-off-by: Arnab Basu <arnab_basu@rocketmail.com>
    Cc: Bhupesh Sharma <bhupesh.sharma@freescale.com>
    Cc: Marc Zyngier <marc.zyngier@arm.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/16359
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Hanna Hawa <hannah@marvell.com>

---
+++ arch/arm/include/asm/armv8/esr.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2015, Arnab Basu <arnab_basu@rocketmail.com>
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#ifndef _ARMV8_ESR_H
+#define _ARMV8_ESR_H
+
+#define ESR_EC_SMC64	(0x17)
+
+#endif /* _ARMV8_ESR_H */

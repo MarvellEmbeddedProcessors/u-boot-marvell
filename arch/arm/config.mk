0eb4e086685ffea289dc70f4acf9b0e86c152c9e
Arnab Basu <arnab_basu@rocketmail.com>
2015-01-13 02:26:28 +0530

    ARMv8: PSCI: Add linker section to hold PSCI code
    A separate linker section makes it possible to keep this code either
    in DDR or in some secure memory location provided specifically for the
    purpose.
    
    So far no one is using this section.
    
    Change-Id: Iccde52e6552ba28e8fb41aa098b2270b8ff053c9
    Signed-off-by: Arnab Basu <arnab_basu@rocketmail.com>
    Cc: Bhupesh Sharma <bhupesh.sharma@freescale.com>
    Cc: Marc Zyngier <marc.zyngier@arm.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/16358
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Hanna Hawa <hannah@marvell.com>

--- arch/arm/config.mk
+++ arch/arm/config.mk
@@ -113,7 +113,7 @@ endif
 
 # limit ourselves to the sections we want in the .bin.
 ifdef CONFIG_ARM64
-OBJCOPYFLAGS += -j .text -j .rodata -j .data -j .u_boot_list -j .rela.dyn
+OBJCOPYFLAGS += -j .text -j .secure_text -j .rodata -j .data -j .u_boot_list -j .rela.dyn
 else
 OBJCOPYFLAGS += -j .text -j .secure_text -j .rodata -j .hash -j .data -j .got.plt -j .u_boot_list -j .rel.dyn
 endif

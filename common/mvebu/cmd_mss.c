78318feb0c49ff9fb8b34fa1aa15df9995ebc9a2
Neta Zur <neta@marvell.com>
2015-01-27 10:34:04 +0200

    cmd: mvebu: mss: add new command line mss_boot
    The mss_boot command line use to load MSS code into CM3 ID RAM and release the CM3 from reset
    Signed-off-by: Neta Zur <neta@marvell.com>
    
    Change-Id: Icde9e2d9e9affd51cf99a598a7b56a0cfebd317b
    Reviewed-on: http://vgitil04.il.marvell.com:8080/16339
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Hanna Hawa <hannah@marvell.com>
    Reviewed-by: Yehuda Yitschak <yehuday@marvell.com>

---
+++ common/mvebu/cmd_mss.c
@@ -0,0 +1,108 @@
+/*
+ * ***************************************************************************
+ * Copyright (C) 2015 Marvell International Ltd.
+ * ***************************************************************************
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 2 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * ***************************************************************************
+ */
+
+#include <common.h>
+#include <asm/io.h>
+#include <command.h>
+
+#define MSS_DMA_SRCBR (MVEBU_MSS_BASE + 0xC0)
+#define MSS_DMA_DSTBR (MVEBU_MSS_BASE + 0xC4)
+#define MSS_DMA_CTRLR (MVEBU_MSS_BASE + 0xC8)
+#define MSS_M3_RSTCR  (MVEBU_MSS_BASE + 0xFC)
+
+#define MSS_DMA_CTRLR_SIZE_OFFSET		(0)
+#define MSS_DMA_CTRLR_SIZE_MASK		    (0xFF)
+#define MSS_DMA_CTRLR_REQ_OFFSET		(15)
+#define MSS_DMA_CTRLR_REQ_SET		    (1)
+#define MSS_DMA_CTRLR_ACK_OFFSET		(12)
+#define MSS_DMA_CTRLR_ACK_MASK		    (0x1)
+#define MSS_DMA_CTRLR_ACK_BUSY		    (0)
+#define MSS_DMA_CTRLR_ACK_READY		    (1)
+#define MSS_M3_RSTCR_RST_OFFSET     (0)
+#define MSS_M3_RSTCR_RST_ON         (0)
+#define MSS_M3_RSTCR_RST_OFF        (1)
+
+#define MSS_DMA_TIMEOUT             1000
+
+int mss_boot(u32 size, u32 srcAddr)
+{
+	u32 i, loop_num, timeout;
+
+	printf("Loading MSS image from address 0x%08X size 0x%x...", (u32)srcAddr, size);
+
+	/* load image to MSS RAM using DMA */
+	loop_num = (size / 128) + (((size & 127) == 0) ? 0 : 1);
+
+	for (i = 0; i < loop_num; i++) {
+		/* write destination and source addresses */
+		writel((srcAddr + (i * 128)), MSS_DMA_SRCBR);
+		writel((i * 128), MSS_DMA_DSTBR);
+
+		/* set the DMA control register */
+		writel(((MSS_DMA_CTRLR_REQ_SET << MSS_DMA_CTRLR_REQ_OFFSET) |
+				(128 << MSS_DMA_CTRLR_SIZE_OFFSET)), MSS_DMA_CTRLR);
+
+		/* Poll DMA_ACK at MSS_DMACTLR until it is ready */
+		timeout = MSS_DMA_TIMEOUT;
+		while (timeout) {
+			if ((readl(MSS_DMA_CTRLR) >> MSS_DMA_CTRLR_ACK_OFFSET & MSS_DMA_CTRLR_ACK_MASK)
+				== MSS_DMA_CTRLR_ACK_READY) {
+				break;
+			}
+			udelay(1);
+			timeout--;
+		}
+		if (timeout == 0) {
+			printf("\nDMA failed to load MSS image\n");
+			return 1;
+		}
+	}
+
+	/* Release M3 from reset */
+	writel((MSS_M3_RSTCR_RST_OFF << MSS_M3_RSTCR_RST_OFFSET), MSS_M3_RSTCR);
+
+	return 0;
+}
+
+
+int do_mss_boot(cmd_tbl_t *cmdtp, int flag, int argc,
+						char *const argv[])
+{
+	u32 srcAddr, size;
+
+	if (argc != 3) {
+		printf("Usage:\n%s\n", cmdtp->usage);
+		return 1;
+	}
+
+	/* Get source address*/
+	srcAddr = simple_strtoul(argv[1], NULL, 16);
+
+	/* Get image size */
+	size = simple_strtoul(argv[2], NULL, 16);
+
+	return mss_boot(size, srcAddr);
+}
+
+U_BOOT_CMD(
+	mss_boot,     3,      1,       do_mss_boot,
+	"mss_boot - Loads MSS code into CM3 ID RAM and release from reset\n",
+	"<image src address> <image size>\n"
+	"	- Loads MSS code into CM3 ID RAM and release CM3 from reset\n"
+);
+

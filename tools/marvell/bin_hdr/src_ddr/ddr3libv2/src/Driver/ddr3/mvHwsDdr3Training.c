ddd6f5abaad89391735e94cf7480d92b9ed747f8
hayim <hayim@marvell.com>
2015-04-15 10:46:58 +0300

    ddr3libv2: added function which returns DDR version string.
    Change-Id: Id42469e9ba4dfda25bafd3c8dd584337d3dfb961
    Signed-off-by: hayim <hayim@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/18288
    Reviewed-by: Igor Patrik <igorp@marvell.com>
    Tested-by: Igor Patrik <igorp@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/24109
    Reviewed-by: Omri Itach <omrii@marvell.com>
    Tested-by: Omri Itach <omrii@marvell.com>

--- tools/marvell/bin_hdr/src_ddr/ddr3libv2/src/Driver/ddr3/mvHwsDdr3Training.c
+++ tools/marvell/bin_hdr/src_ddr/ddr3libv2/src/Driver/ddr3/mvHwsDdr3Training.c
@@ -153,13 +153,6 @@ GT_U32 maskTuneFunc = (SET_MEDIUM_FREQ_MASK_BIT     |
                        CENTRALIZATION_RX_MASK_BIT   |
                        CENTRALIZATION_TX_MASK_BIT   );
 
-void ddr3PrintVersion()
-{
-	mvPrintf(DDR3_TIP_VERSION_STRING);
-}
-
-
-
 extern GT_U32 isPllOld;
 extern ClValuePerFreq casLatencyTable[];
 extern PatternInfo patternTable[] ;
@@ -314,7 +307,6 @@ static RegData odpgDefaultValue[]=
     {0x16fc, 0x0,       MASK_ALL_BITS}     
 };
 
-
 /**************************** internal function header ************************/
 GT_STATUS ddr3TipEnableInitSequence(GT_U32 devNum);
 
@@ -335,6 +327,21 @@ static GT_STATUS    ddr3TipBusAccess
 
 static GT_STATUS ddr3TipPadInv(GT_U32 devNum, GT_U32 interfaceId);
 static GT_STATUS ddr3TipRankControl(GT_U32 devNum, GT_U32 interfaceId);
+
+/*****************************************************************************
+Version
+******************************************************************************/
+
+const GT_CHAR* mvHwsDdr3TipVersionGet(void)
+{
+    return DDR3_TIP_VERSION_STRING;
+}
+
+void ddr3PrintVersion()
+{
+    mvPrintf(mvHwsDdr3TipVersionGet());
+    mvPrintf("\n");
+}
 
 /*****************************************************************************
 Register freq mask
@@ -2160,6 +2167,7 @@ GT_STATUS    ddr3TipDynamicOdt
         mr1Value = 0;
         mr2Value = 0x200;
     }
+
     /* MR1: set RttNom to RZQ/6 */
     CHECK_STATUS(mvHwsDdr3TipIFWrite(devNum, ACCESS_TYPE_MULTICAST, 0, MR1_REG, mr1Value, 0x224));
     /* MR2: disable dynamic ODT*/

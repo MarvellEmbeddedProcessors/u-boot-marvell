7c9033b9e956e3ab5ddebeed4fb9c43903f5e36c
Bassel Saba <basselsa@marvell.com>
2015-04-16 18:56:23 +0300

    fix: spi: msys, a38x: fixed SPI mtdparts configuration
    	- mtdparts default value wasn't correct, that caused LSP 3.4.69
    	  to read it incorrectly (had extra 'mtdparts=' string in it),
    	  and for LSP 3.10.x "spi_flash" was used instead of "spi0.0"
    	  that failed Linux to read it.
    	- fixed default value.
    
    Change-Id: I27c974d62d3ea1d4ab5a75d8ac93bcfa23fb62b9
    Signed-off-by: Bassel Saba <basselsa@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/18371
    Reviewed-by: Omri Itach <omrii@marvell.com>
    Tested-by: Omri Itach <omrii@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/18528
    Tested-by: Star_Automation <star@marvell.com>

--- board/mv_ebu/common/USP/mv_main.c
+++ board/mv_ebu/common/USP/mv_main.c
@@ -302,7 +302,7 @@ void misc_init_r_env(void){
 
 	envSetDefault("console", "console=ttyS0,115200");
 #if defined(MV_NAND) && defined(MV_INCLUDE_SPI)
-	envSetDefault("mtdparts", "'mtdparts=armada-nand:8m(boot)ro,8m@8m(kernel),-(rootfs);mtdparts=spi_flash:4m(boot),-(spi-rootfs)'");
+	envSetDefault("mtdparts", "'mtdparts=armada-nand:8m(boot)ro,8m@8m(kernel),-(rootfs);spi_flash:4m(boot),-(spi-rootfs)'");
 	envSetDefault("mtdids", "nand0=armada-nand;spi0=spi_flash");
 #elif defined(MV_NAND)
 	envSetDefault("mtdparts", "mtdparts=armada-nand:8m(boot)ro,8m@8m(kernel),-(rootfs)");

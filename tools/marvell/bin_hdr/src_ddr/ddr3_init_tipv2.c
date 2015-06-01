5b7451aaadad9b66b40b81e7610bd1cb5f518d7a
Igor Patrik <igorp@marvell.com>
2015-06-01 14:05:40 +0300

    ddr: Fixed commenented out debug level
    	Mistake in name
    
    Change-Id: Ib40c0929ca8be6f4373c34c791eab97a5f10c0e1
    Signed-off-by: Igor Patrik <igorp@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/19655
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Omri Itach <omrii@marvell.com>
    Tested-by: Omri Itach <omrii@marvell.com>

--- tools/marvell/bin_hdr/src_ddr/ddr3_init_tipv2.c
+++ tools/marvell/bin_hdr/src_ddr/ddr3_init_tipv2.c
@@ -411,7 +411,7 @@ MV_U32 ddr3Init(void)
 
 	/*Set log level for training lib*/
 	ddr3HwsSetLogLevel(MV_DEBUG_BLOCK_ALL, DEBUG_LEVEL_ERROR);
-	/*ddr3HwsSetLogLevel(MV_DEBUG_TRAINING_MAIN, DEBUG_LEVEL_TRACE);*/
+	/*ddr3HwsSetLogLevel(MV_DEBUG_BLOCK_TRAINING_MAIN, DEBUG_LEVEL_TRACE);*/
 
 #ifdef MV_RUN_WIN_VALIDATION_TEST /* to run DDR viewer tool (to display DDR training results) */
 	mvHwsDdr3TipSweepTest(MV_TRUE);

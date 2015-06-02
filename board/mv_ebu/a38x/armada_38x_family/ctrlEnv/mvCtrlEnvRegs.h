57b785cb07ae6757d222f6cb64844f6e5736b5d9
Omri Itach <omrii@marvell.com>
2015-06-02 12:01:45 +0300

    a38x: add support for S@R CPU/DDR mode 0x13: A-sync 2000/933MHz
    Change-Id: I1a5b333b863b0002d296618f501097405a2e339d
    Signed-off-by: Omri Itach <omrii@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/19693
    Tested-by: Star_Automation <star@marvell.com>

--- board/mv_ebu/a38x/armada_38x_family/ctrlEnv/mvCtrlEnvRegs.h
+++ board/mv_ebu/a38x/armada_38x_family/ctrlEnv/mvCtrlEnvRegs.h
@@ -417,6 +417,7 @@ typedef struct {
 	{ 0x8,   1332, 666,  666,  MV_TRUE  , MV_FALSE}, \
 	{ 0xC,   1600, 800,  800,  MV_TRUE  , MV_FALSE}, \
 	{ 0x10,  1866, 933,  933,  MV_TRUE  , MV_TRUE}, \
+	{ 0x13,  2000, 933,  1000,  MV_TRUE  , MV_TRUE}, \
 	{ MV_SAR_FREQ_MODES_EOT,  0,    0,   0, MV_FALSE, MV_FALSE } \
 };
 #else /* CONFIG_ARMADA_39x */
@@ -443,6 +444,7 @@ typedef struct {
 	{ 0x8,   1332, 666,  666,  MV_TRUE  , MV_FALSE}, \
 	{ 0xC,   1600, 800,  800,  MV_TRUE  , MV_FALSE}, \
 	{ 0x10,  1866, 933,  933,  MV_TRUE  , MV_TRUE}, \
+	{ 0x13,  2000, 933,  1000,  MV_TRUE  , MV_TRUE}, \
 	{ MV_SAR_FREQ_MODES_EOT,  0,    0,   0, MV_FALSE, MV_FALSE } \
 };
 #else /* CONFIG_ARMADA_39x */

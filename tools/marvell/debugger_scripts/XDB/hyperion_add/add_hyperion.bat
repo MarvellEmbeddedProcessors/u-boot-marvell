d32df742d93e4e869877838028803c88b12426af
kostap <kostap@marvell.com>
2015-07-14 14:48:50 +0300

    tools: Add new Hyperion XDB scripts
    	- Add new Hyperion scrypt supporting 2 CPUs (2xCA53+CM3)
    	- Add Linux probe connection option to old low-level UP script
    
    Change-Id: I19b50062b61623987d4023468f7002b5e2f7bd63
    Signed-off-by: kostap <kostap@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/21271
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Hanna Hawa <hannah@marvell.com>

---
+++ tools/marvell/debugger_scripts/XDB/hyperion_add/add_hyperion.bat
@@ -0,0 +1,18 @@
+
+@echo off
+
+echo Add Hyperion to XDB in %MARVELL_XDB_HOME%
+
+set THOME=%MARVELL_XDB_HOME%\xdb\tci\cs\jtag
+
+initool "%THOME%\config1\jtag.ini" -m jtag1.ini
+initool "%THOME%\config2\jtag.ini" -m jtag2.ini
+initool "%THOME%\config2\jtag.ini" -m jtag3.ini
+
+set THOME=%MARVELL_XDB_HOME%xdb\configurations\jtag\SoC\hyperion
+if not exist "%THOME%" mkdir "%THOME%"
+
+copy .\hyperion_3core.xsf "%THOME%"
+
+@echo on
+
---
+++ tools/marvell/debugger_scripts/XDB/hyperion_add/add_hyperion.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ -z "$MARVELL_XDB_HOME" ]; then
+	#	MARVELL_XDB_HOME="/home/user/.wine/drive_c/Marvell/XDB5.7"
+	echo "Please set the XDB location by setting MARVELL_XDB_HOME variable"
+	exit 1
+fi
+echo "Add Hyperion to XDB in $MARVELL_XDB_HOME"
+
+THOME=$MARVELL_XDB_HOME/xdb/tci/cs/jtag
+
+if [ -z `grep -m 1 hyperion "$THOME/config1/jtag.ini"` ]; then
+	cat jtag1.ini >> "$THOME/config1/jtag.ini"
+	echo "Adding Hyperion settings to $THOME/config1/jtag.ini file"
+else
+	echo "Hyperion settings already present in $THOME/config1/jtag.ini file"
+fi
+if [ -z `grep -m 1 hyperion "$THOME/config2/jtag.ini"` ]; then
+	cat jtag2.ini >> "$THOME/config2/jtag.ini"
+	echo "Adding Hyperion settings to $THOME/config2/jtag.ini file"
+else
+	echo "Hyperion settings already present in $THOME/config1/jtag.ini file"
+fi
+if [ -z `grep -m 1 hyperion "$THOME/config3/jtag.ini"` ]; then
+	cat jtag3.ini >> "$THOME/config3/jtag.ini"
+	echo "Adding Hyperion settings to $THOME/config2/jtag.ini file"
+else
+	echo "Hyperion settings already present in $THOME/config1/jtag.ini file"
+fi
+
+THOME=$MARVELL_XDB_HOME/xdb/configurations/jtag/SoC/hyperion
+
+if  ! [ -d "$THOME" ]; then
+	mkdir "$THOME"
+	echo "Creating new folder $THOME"
+fi
+
+cp ../hyperion_3core.xsf "$THOME"
+
+

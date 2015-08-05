c30d34dffe95218b63813689bb0ed1b60f9bc2c7
Margarita Granov <margra@marvell.com>
2015-08-05 10:38:56 +0300

    ddr3libv2: Bump DDR3 TIP-1.40 and DDR4 TIP-0.20
        BobK integration changes
    
    Change-Id: I5d96ad553d6b57482a8fbae1d7894b3c4e6f2697
    Signed-off-by: Margarita Granov <margra@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/22531
    Reviewed-by: Haim Boot <hayim@marvell.com>
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/24148
    Reviewed-by: Omri Itach <omrii@marvell.com>

--- tools/marvell/bin_hdr/src_ddr/ddr3libv2/h/Driver/ddr3/mvDdr3TrainingIp.h
+++ tools/marvell/bin_hdr/src_ddr/ddr3libv2/h/Driver/ddr3/mvDdr3TrainingIp.h
@@ -52,9 +52,9 @@
 #endif
 
 #ifdef CONFIG_DDR4
-#define DDR3_TIP_VERSION_STRING "DDR4 Training Sequence - Ver TIP-0.19."
+#define DDR3_TIP_VERSION_STRING "DDR4 Training Sequence - Ver TIP-0.20."
 #else
-#define DDR3_TIP_VERSION_STRING "DDR3 Training Sequence - Ver TIP-1.39."
+#define DDR3_TIP_VERSION_STRING "DDR3 Training Sequence - Ver TIP-1.40."
 #endif
 
 #define MAX_CS_NUM         (4)

4abd15744035901b856497969ca8d6d23987e792
Shaker Daibes <shaker@marvell.com>
2015-08-17 13:28:02 +0300

    Bump U-Boot 2013.01 To release 2015_T3.0.eng_drop_v4
    Change-Id: I9d6c0476ca4dec816faa89219bb5f0a3121c5879
    Signed-off-by: Shaker Daibes <shaker@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/22892
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Hanna Hawa <hannah@marvell.com>

--- include/configs/mv_version.h
+++ include/configs/mv_version.h
@@ -39,6 +39,6 @@
 /*
  * Version
  */
-#define CONFIG_IDENT_STRING	" Marvell version: 2015_T3.0.eng_drop_v3"
+#define CONFIG_IDENT_STRING	" Marvell version: 2015_T3.0.eng_drop_v4"
 
 #endif /* __MV_VERSION_H */

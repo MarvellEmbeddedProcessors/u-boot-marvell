1ddcc90051cfe51388a709fadb23da2218c68ecd
Hanna Hawa <hannah@marvell.com>
2015-10-14 10:38:23 +0300

    Bump U-Boot 2015.01 To release 2015_T3.0.eng_drop_v3
    Change-Id: Ic5db37d51a69588ae008c28fcfd63845036aa5bb
    Signed-off-by: Hanna Hawa <hannah@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/23986
    Tested-by: Star_Automation <star@marvell.com>

--- include/configs/mvebu-common.h
+++ include/configs/mvebu-common.h
@@ -22,7 +22,7 @@
 #include <asm/arch/regs-base.h>
 
 /* Version */
-#define CONFIG_IDENT_STRING             "  Marvell version: 2015_T3.0.eng_drop_v2"
+#define CONFIG_IDENT_STRING             "  Marvell version: 2015_T3.0.eng_drop_v3"
 
 /* Temp */
 #define CONFIG_ADEC

41a34fd4097f6a1c34c99dba351399613a41be53
hayim <hayim@marvell.com>
2015-10-21 09:37:22 +0300

    ddr3libv2: DDR4 TIP-0.22
    Change-Id: I65ba3948554b29030e3d6f9dd720c7aa110f6e91
    Signed-off-by: hayim <hayim@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/24182
    (cherry picked from commit a7c28f874488cd9533c17829ab7e966591bd5a18)
    Reviewed-on: http://vgitil04.il.marvell.com:8080/24294
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Omri Itach <omrii@marvell.com>

--- tools/marvell/bin_hdr/src_ddr/ddr3libv2/h/Driver/ddr3/mvDdr3TrainingIp.h
+++ tools/marvell/bin_hdr/src_ddr/ddr3libv2/h/Driver/ddr3/mvDdr3TrainingIp.h
@@ -52,7 +52,7 @@
 #endif
 
 #ifdef CONFIG_DDR4
-#define DDR3_TIP_VERSION_STRING "DDR4 Training Sequence - Ver TIP-0.21."
+#define DDR3_TIP_VERSION_STRING "DDR4 Training Sequence - Ver TIP-0.22."
 #else
 #define DDR3_TIP_VERSION_STRING "DDR3 Training Sequence - Ver TIP-1.46."
 #endif

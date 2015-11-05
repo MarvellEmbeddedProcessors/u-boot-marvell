df1372ba6c8610b9dcfc0679f0c12cf961971b90
Hanna Hawa <hannah@marvell.com>
2015-11-05 14:37:09 +0200

    Bump U-Boot 2013.01 To release 2015_T3.0.eng_drop_v12
    Change-Id: I24b79d2403e20362d9c3556b97add4a96001eb5d
    Signed-off-by: Hanna Hawa <hannah@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/24595
    Tested-by: Star_New_DDR <star-new-ddr@marvell.com>
    Tested-by: Star_Automation <star@marvell.com>

--- include/configs/mv_version.h
+++ include/configs/mv_version.h
@@ -39,6 +39,6 @@
 /*
  * Version
  */
-#define CONFIG_IDENT_STRING	" Marvell version: 2015_T3.0.eng_drop_v11"
+#define CONFIG_IDENT_STRING	" Marvell version: 2015_T3.0.eng_drop_v12"
 
 #endif /* __MV_VERSION_H */

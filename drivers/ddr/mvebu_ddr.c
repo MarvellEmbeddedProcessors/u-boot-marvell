892e92a47ef42ded2d8f67eccb84a0af7d36f790
Shadi Ammouri <shadi@marvell.com>
2015-11-17 12:43:14 +0200

    ddr: Pass DDR configuration parameters from FDT
    	- Use FDT to pass cs-count, width and size of DDR.
    	- This is the framework to which additional DDR porting parameters will
    	  be added later on.
    
    Change-Id: If78484d139d30a5c5bca2c49417c365890481e3f
    Signed-off-by: Shadi Ammouri <shadi@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/24954
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Hanna Hawa <hannah@marvell.com>

--- drivers/ddr/mvebu_ddr.c
+++ drivers/ddr/mvebu_ddr.c
@@ -40,8 +40,11 @@ static void static_dram_init(struct mvebu_dram_config *config)
 void mvebu_dram_init(const void *blob)
 {
 	u32 node;
+	int sub_node, cs_cnt;
 	struct mvebu_dram_config dram_config;
 	struct mvebu_dram_config *config = &dram_config;
+	u64 size_64;
+
 	debug_enter();
 
 	/* Get DDR MAC node from the FDT blob */
@@ -57,7 +60,28 @@ void mvebu_dram_init(const void *blob)
 		return;
 	}
 
-	/* Get DDR MAC node from the FDT blob */
+	config->cs_count = fdtdec_get_int(blob, node, "cs_count", 0);
+	config->bus_width = fdtdec_get_int(blob, node, "bus_width", 0);
+	config->size_mb = 0x0;
+
+	/* Get configuration info for each of the DRAM CS's */
+	sub_node = fdt_first_subnode(blob, node);
+	cs_cnt = 0;
+	if (sub_node >= 0) {
+		do {
+			cs_cnt++;
+			size_64 = fdtdec_get_uint64(blob, sub_node, "size", 0x0);
+			/* Convert to MB, and accumulate */
+			config->size_mb += (size_64 >> 20);
+			sub_node = fdt_next_subnode(blob, sub_node);
+		} while (sub_node > 0);
+		if (cs_cnt < config->cs_count)
+			error("Missing configuration for DRAM CS %d to %d.\n", cs_cnt + 1, config->cs_count);
+	} else {
+		error("No configuration per DRAM CS was found\n");
+	}
+
+	/* Get DDR PHY node from the FDT blob */
 	node = fdt_node_offset_by_compatible(blob, -1, fdtdec_get_compatible(COMPAT_MVEBU_DDR_PHY));
 	if (node == -1) {
 		error("No DDR PHY node found in FDT blob\n");
@@ -69,11 +93,6 @@ void mvebu_dram_init(const void *blob)
 		error("missing DDR PHY base address in DDR PHY node\n");
 		return;
 	}
-
-	/* Use default DRAM params, till we get the values from DT. */
-	config->cs_count = 0;
-	config->bus_width = 0;
-	config->size_mb = 0;
 
 #ifdef CONFIG_MVEBU_STATIC_SUPPORT
 	static_dram_init(config);

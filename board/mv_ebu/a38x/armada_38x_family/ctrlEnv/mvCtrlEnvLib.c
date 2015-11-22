1c1acc54e9219e88692c7174b396b8ccb9c672f0
Bassel Saba <basselsa@marvell.com>
2015-11-22 13:19:28 +0200

    fix: thermal: a38x, a39x: band gap circuit curve errata #132698 update
    the errata fix was performed before an errata number was ready,
    this commit adds the errata number to source code only.
    
    Change-Id: I3d478fb270379a296d3ec94435ee0c74068b6d19
    Signed-off-by: Bassel Saba <basselsa@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/25135
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Lior Amsalem <alior@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/25295
    Reviewed-by: Omri Itach <omrii@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/25450

--- board/mv_ebu/a38x/armada_38x_family/ctrlEnv/mvCtrlEnvLib.c
+++ board/mv_ebu/a38x/armada_38x_family/ctrlEnv/mvCtrlEnvLib.c
@@ -2371,7 +2371,7 @@ MV_U32 mvCtrlGetJuncTemp(MV_VOID)
 	/* Initiates TSEN hardware reset once */
 	if ((MV_REG_READ(TSEN_CONTROL_MSB_REG) & TSEN_CONTROL_MSB_RST_MASK) == 0) {
 		MV_REG_BIT_SET(TSEN_CONTROL_MSB_REG, TSEN_CONTROL_MSB_RST_MASK);
-		/* set TSEN TC Trim value */
+		/* set Tsen Tc Trim to correct default value (errata #132698) */
 		reg = MV_REG_READ(TSEN_CONTROL_LSB_REG);
 		reg &= ~TSEN_CONTROL_LSB_TC_TRIM_MASK;
 		reg |= 0x3 << TSEN_CONTROL_LSB_TC_TRIM_OFFSET;

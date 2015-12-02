178460ca072191672b559bbbe6db4d9d8d7953ac
Yaron David <yarond@marvell.com>
2015-12-02 15:14:36 +0200

    ddr3libv2: Bump DDR3 TIP-1.53
    Change-Id: I4b0dc8c63a1be90e1da63ffd4f529d1abc1523ac
    Signed-off-by: Yaron David <yarond@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/25526

--- tools/marvell/bin_hdr/src_ddr/ddr3libv2/h/Driver/ddr3/mvDdr3TrainingIp.h
+++ tools/marvell/bin_hdr/src_ddr/ddr3libv2/h/Driver/ddr3/mvDdr3TrainingIp.h
@@ -54,7 +54,7 @@
 #ifdef CONFIG_DDR4
 #define DDR3_TIP_VERSION_STRING "DDR4 Training Sequence - Ver TIP-0.23."
 #else
-#define DDR3_TIP_VERSION_STRING "DDR3 Training Sequence - Ver TIP-1.52."
+#define DDR3_TIP_VERSION_STRING "DDR3 Training Sequence - Ver TIP-1.53."
 #endif
 
 #define MAX_CS_NUM         (4)

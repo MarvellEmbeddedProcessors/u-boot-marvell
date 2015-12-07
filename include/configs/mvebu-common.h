e88254c9b9056509e8d577d0389fdef470833aec
Nizan Zorea <nzorea@marvell.com>
2015-12-07 18:55:22 +0200

    fdt: add support for multi Device Tree support
    	- This patch added support for multi FDT support
    	- To enable this feature need to enable CONFIG_MULTI_DT_FILE config
    	  and set the list of the device tree blob files
    	- This feature is enabled for AP-806 DB board.
    
    Change-Id: I5144f8dbcd936cb8c0b0c5dc87717a03f3661035
    Signed-off-by: Nizan Zorea <nzorea@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/25693
    Reviewed-by: Hanna Hawa <hannah@marvell.com>
    Tested-by: Hanna Hawa <hannah@marvell.com>

--- include/configs/mvebu-common.h
+++ include/configs/mvebu-common.h
@@ -218,6 +218,7 @@
 
 /* Flat Device Tree Definitions */
 #define CONFIG_OF_LIBFDT
+#define MVEBU_FDT_SIZE 0x2000
 
 /* SMP Spin Table Definitions */
 #define CPU_RELEASE_ADDR               (CONFIG_SYS_SDRAM_BASE + 0x2000000)

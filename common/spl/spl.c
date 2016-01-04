18a42de95aca62b3ef27f9d8c5073e16970a43c2
Hanna Hawa <hannah@marvell.com>
2016-01-04 12:48:25 +0200

    fix: mvebu: added Marvell version for SPL print
    - SYSTEMSW-2036
    
    Change-Id: Id73bc023ff52b726ab47d229c2aa1383ab5c6610
    Signed-off-by: Hanna Hawa <hannah@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/26469
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Omri Itach <omrii@marvell.com>

--- common/spl/spl.c
+++ common/spl/spl.c
@@ -271,7 +271,7 @@ void preloader_console_init(void)
 	gd->have_console = 1;
 
 	puts("\nU-Boot SPL " PLAIN_VERSION " (" U_BOOT_DATE " - " \
-			U_BOOT_TIME ")\n");
+			U_BOOT_TIME ")" CONFIG_IDENT_STRING "\n");
 #ifdef CONFIG_SPL_DISPLAY_PRINT
 	spl_display_print();
 #endif

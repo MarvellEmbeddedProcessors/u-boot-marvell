508c28cb95d5a314794902b5323e51fec48d978b
Josef Mously <myossi@marvell.com>
2016-03-24 14:40:22 +0200

    fix: a38x: fixed bootcmd usage in boot commands
    following patch I43aa6fe72eb52a68b6d4e7c5169e92972b5947ef,
    adding gatewayip and netdev to all bootcmd related commands,
    to avoid duplicated settings.
    
    JIRA: SYSTEMSW-1685
    
    Change-Id: Id1900463b4d363b5c6d5226f01886bae204a0507
    Signed-off-by: Josef Mously <myossi@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/28505
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Lior Amsalem <alior@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/33297
    Reviewed-by: Hanna Hawa <hannah@marvell.com>

--- board/mv_ebu/a38x/mv_main_a38x.c
+++ board/mv_ebu/a38x/mv_main_a38x.c
@@ -516,14 +516,16 @@ void misc_init_r_env(void)
 #endif
 	env = getenv("bootcmd_auto");
 	if (!env)
-		setenv("bootcmd_auto", "stage_boot $boot_order");
+		setenv("bootcmd_auto", "setenv bootargs_end :$gatewayip:255.255.255.0:Armada:$netdev:none; "
+				"stage_boot $boot_order");
 	env = getenv("bootcmd_lgcy");
 	if (!env)
-		setenv("bootcmd_lgcy", "tftpboot 0x2000000 $image_name;"
-			   "setenv bootargs $console $nandEcc $mtdparts_lgcy"
-			   " $bootargs_root nfsroot=$serverip:$rootpath "
-			   "ip=$ipaddr:$serverip$bootargs_end  video=dovefb:lcd0:$lcd0_params "
-			   "clcd.lcd0_enable=$lcd0_enable clcd.lcd_panel=$lcd_panel;  bootm $loadaddr; ");
+		setenv("bootcmd_lgcy", "setenv bootargs_end :$gatewayip:255.255.255.0:Armada:$netdev:none; "
+			"tftpboot 0x2000000 $image_name;"
+			"setenv bootargs $console $nandEcc $mtdparts_lgcy"
+			" $bootargs_root nfsroot=$serverip:$rootpath "
+			"ip=$ipaddr:$serverip$bootargs_end  video=dovefb:lcd0:$lcd0_params "
+			"clcd.lcd0_enable=$lcd0_enable clcd.lcd_panel=$lcd_panel;  bootm $loadaddr; ");
 #endif
 	env = getenv("pxe_files_load");
 	if (!env)
@@ -705,7 +707,8 @@ void misc_init_r_env(void)
 #if (CONFIG_BOOTDELAY >= 0)
 	env = getenv("bootcmd");
 	if (!env)
-		setenv("bootcmd", "tftpboot 0x2000000 $image_name;tftpboot $fdtaddr $fdtfile;"
+		setenv("bootcmd", "setenv bootargs_end :$gatewayip:255.255.255.0:Armada:$netdev:none; "
+		"tftpboot 0x2000000 $image_name;tftpboot $fdtaddr $fdtfile;"
 		"setenv bootargs $console $nandEcc $mtdparts $bootargs_root nfsroot=$serverip:$rootpath "
 		"ip=$ipaddr:$serverip$bootargs_end $mvNetConfig video=dovefb:lcd0:$lcd0_params "
 		"clcd.lcd0_enable=$lcd0_enable clcd.lcd_panel=$lcd_panel; bootz 0x2000000 - $fdtaddr;");
--- board/mv_ebu/common/USP/mv_fdt.c
+++ board/mv_ebu/common/USP/mv_fdt.c
@@ -66,7 +66,7 @@ void fdt_env_setup(char *fdtfile, MV_BOOL runUpdate)
 		setenv("fdtfile", fdtfile);
 
 	/* boot command to fetch DT file, update DT (if fdt_skip_update=no) and bootz LSP zImage */
-	char bootcmd_fdt[] = "setenv bootargs_end :$gatewayip:255.255.255.0:Armada:$netdev:none;"
+	char bootcmd_fdt[] = "setenv bootargs_end :$gatewayip:255.255.255.0:Armada:$netdev:none; "
 		"tftpboot 0x2000000 $image_name;tftpboot $fdtaddr $fdtfile;"
 		"setenv bootargs $console $nandEcc $mtdparts $bootargs_root nfsroot=$serverip:$rootpath "
 		"ip=$ipaddr:$serverip$bootargs_end $mvNetConfig video=dovefb:lcd0:$lcd0_params "
@@ -75,7 +75,8 @@ void fdt_env_setup(char *fdtfile, MV_BOOL runUpdate)
 	/* boot command to and bootz LSP zImage (after DT already fetch and set) */
 	env = getenv("bootcmd_fdt_boot");
 	if (!env)
-		setenv("bootcmd_fdt_boot", "tftpboot 0x2000000 $image_name; setenv bootargs $console $nandEcc "
+		setenv("bootcmd_fdt_boot", "setenv bootargs_end :$gatewayip:255.255.255.0:Armada:$netdev:none; "
+			"tftpboot 0x2000000 $image_name; setenv bootargs $console $nandEcc "
 			"$mtdparts $bootargs_root nfsroot=$serverip:$rootpath ip=$ipaddr:$serverip$bootargs_end "
 			"$mvNetConfig video=dovefb:lcd0:$lcd0_params clcd.lcd0_enable=$lcd0_enable "
 			"clcd.lcd_panel=$lcd_panel; bootz 0x2000000 - $fdtaddr;");
@@ -83,7 +84,8 @@ void fdt_env_setup(char *fdtfile, MV_BOOL runUpdate)
 	/* boot command to fetch DT file, and set bootcmd_fdt_boot as new bootcmd (manually edit DT & run 'boot') */
 	env = getenv("bootcmd_fdt_edit");
 	if (!env)
-		setenv("bootcmd_fdt_edit", "tftpboot $fdtaddr $fdtfile; fdt addr $fdtaddr; setenv bootcmd $bootcmd_fdt_boot");
+		setenv("bootcmd_fdt_edit", "setenv bootargs_end :$gatewayip:255.255.255.0:Armada:$netdev:none; "
+			"tftpboot $fdtaddr $fdtfile; fdt addr $fdtaddr; setenv bootcmd $bootcmd_fdt_boot");
 
 	env = getenv("bootcmd_fdt");
 	if (!env)

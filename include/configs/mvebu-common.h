9e881e77d6ab2f36400f8b236a46e755eeb7440d
jinghua <jinghua@marvell.com>
2016-04-16 01:45:21 +0800

    Bump U-Boot 2015.01 To release devel-16.04.3
    Change-Id: I8e4b8c60b6bcce5e6d60fecd4f685b7cf36b9004
    Signed-off-by: jinghua <jinghua@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/29089
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Haim Boot <hayim@marvell.com>

--- include/configs/mvebu-common.h
+++ include/configs/mvebu-common.h
@@ -22,7 +22,7 @@
 #include <asm/arch/regs-base.h>
 
 /* Version */
-#define CONFIG_IDENT_STRING             "  Marvell version: devel-16.04.2"
+#define CONFIG_IDENT_STRING             "  Marvell version: devel-16.04.3"
 
 /* Temp */
 #define CONFIG_ADEC

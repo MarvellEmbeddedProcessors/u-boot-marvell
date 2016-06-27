b4ca76cb6c9380bbd81fd2be075fa4c5d7d75f68
Terry Zhou <bjzhou@marvell.com>
2016-06-27 17:43:40 +0300

    config: a3700: update the name of db to ddr3-db
    Change-Id: Ic61821e681351440cae2d2b037eec9a8a2be5a14
    Signed-off-by: Terry Zhou <bjzhou@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/30755
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Hua Jing <jinghua@marvell.com>
    Reviewed-by: Wilson Ding <dingwei@marvell.com>
    Verified-Armada37x0: Wilson Ding <dingwei@marvell.com>

--- include/dt-bindings/multi-fdt/multi-fdt.h
+++ include/dt-bindings/multi-fdt/multi-fdt.h
@@ -19,12 +19,11 @@
 #ifndef _MULTI_FDT_H_
 #define _MULTI_FDT_H_
 
-#define A3700_DB_ID			0x20
+#define A3700_DDR3_DB_ID		0x20
 #define A3700_DDR4_DB_ID		0x21
 /*
- * There are three main presets for Armada3700 DB, between
+ * There are three main presets for Armada3700 DDR3 DB, between
  * these three presets, most of the configurations are the same:
-   - CPU 800MHz and DDR 800MHz
    - SATA
    - neta0 - RGMII
    - PCIe(COMPHY-0)
@@ -34,13 +33,12 @@
  * preset 2. COMPHY-1 is working as SGMII-1 connected with PHY
  * preset 3. COMPHY-1 is working as SGMII-1 connected with SFP2.5G
  */
-#define A3700_DB_CONFIG_ID_USB3		1
-#define A3700_DB_CONFIG_ID_SGMII1	2
-#define A3700_DB_CONFIG_ID_SFP_2_5G	3
+#define A3700_DDR3_DB_CONFIG_ID_USB3		1
+#define A3700_DDR3_DB_CONFIG_ID_SGMII1		2
+#define A3700_DDR3_DB_CONFIG_ID_SFP_2_5G	3
 
 /*
  * Add new preset for Armada3700 DDR4 DB
-   - CPU 1000MHz and DDR4 800MHz
    - SATA
    - neta0 - RGMII
    - PCIe(COMPHY-0)
@@ -48,7 +46,7 @@
  */
 #define A3700_DDR4_DB_CONFIG_ID_USB3 4
 
-#define A3700_DB_CONFIG_ID_DEFAULT	A3700_DB_CONFIG_ID_USB3
+#define A3700_DDR3_DB_CONFIG_ID_DEFAULT	A3700_DDR3_DB_CONFIG_ID_USB3
 #define A3700_DDR4_DB_CONFIG_ID_DEFAULT	A3700_DDR4_DB_CONFIG_ID_USB3
 
 #endif /* _MULTI_FDT_H_ */

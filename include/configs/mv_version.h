4a5eef5bf62d92d9863ecaace1e0a6f4ddbcd4cb
Josef Mously <myossi@marvell.com>
2016-06-28 11:36:49 +0300

    Bump U-Boot 2013.01 To release 2016_T1.0.eng_drop_v9
    Change-Id: If353cb7cb8527877ff461595c96f349331d537ff
    Signed-off-by: Josef Mously <myossi@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/30766
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Lior Amsalem <alior@marvell.com>

--- include/configs/mv_version.h
+++ include/configs/mv_version.h
@@ -39,6 +39,6 @@
 /*
  * Version
  */
-#define CONFIG_IDENT_STRING	" Marvell version: 2016_T1.0.eng_drop_v8"
+#define CONFIG_IDENT_STRING	" Marvell version: 2016_T1.0.eng_drop_v9"
 
 #endif /* __MV_VERSION_H */

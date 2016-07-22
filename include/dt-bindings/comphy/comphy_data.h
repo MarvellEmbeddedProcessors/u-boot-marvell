568f6138d1e18ba0b40918ac606647087956dc88
zachary <zhangzg@marvell.com>
2016-07-23 01:06:59 +0800

    comphy: a3700, a70x0: add utmi connection type definition for usb2
    - For armada3700, there is a dedicate UTMI PHY for USB2 host only.
    - Add UTMI connection type for USB2 host for it in comphy header file.
    
    Change-Id: Ibb104f26158360d5c5a254806df0b1223a84534f
    Signed-off-by: zachary <zhangzg@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/31353
    Reviewed-by: Hua Jing <jinghua@marvell.com>
    Reviewed-by: Haim Boot <hayim@marvell.com>
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Wilson Ding <dingwei@marvell.com>

--- include/dt-bindings/comphy/comphy_data.h
+++ include/dt-bindings/comphy/comphy_data.h
@@ -67,6 +67,7 @@
 #define UTMI_PHY_TO_USB3_HOST0		0
 #define UTMI_PHY_TO_USB3_HOST1		1
 #define UTMI_PHY_TO_USB3_DEVICE0	2
+#define UTMI_PHY_TO_USB2_HOST0		3
 #define UTMI_PHY_INVALID		0xff
 
 #endif /* _COMPHY_DATA_H_ */

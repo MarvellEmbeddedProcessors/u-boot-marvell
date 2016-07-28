bf5fc5a0f1dcb8934b3e44c1979b089e0df2c745
Terry Zhou <bjzhou@marvell.com>
2016-07-28 14:08:33 +0800

    fix: usb: enable vbus toggle instead of usb vbus wa
    - We need to set usb3 VBUS to HIGH in usb start, currently, we
      call a weak function "board_usb_vbus_init". This is only a WA,
      the best way is to call the "usb_vbus_toggle" which relies on
      the device tree node.
    - Add the definition of gpio-vbus for usb3 node, which was controlled
      by PCA953x io_expander, bit 1. after that, the WA is no longer
      needed.
    - Set the enable/disabel delay to 1000ms according to
      our test:
      - SYSTEMSW-2767
      - we have removed the big capacitor at the VBUS signal
        which shorted the disable delay from 3s to 1s
      - some usb device (like ADATA-UV128/64GB stick) needs more
        delay after enable VBUS, change enable delay from 500ms to 1s.
    
    Change-Id: I5cd10afff358fb2fa409e31ddb24d763e0bac992
    Signed-off-by: Terry Zhou <bjzhou@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/30441
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Wilson Ding <dingwei@marvell.com>

--- board/mvebu/armada3700/devel-board.c
+++ board/mvebu/armada3700/devel-board.c
@@ -59,60 +59,6 @@ void board_ahci_power_on(void)
 }
 #endif
 
-#ifdef CONFIG_USB_XHCI
-/* Set USB VBUS signals (via I2C IO expander/GPIO) as output and set output value as enabled */
-void board_usb_vbus_init(void)
-{
-/* This I2C IO expander configuration is board specific, only to Marvell A3700 DB board.
- * (I2C device at address 0x22, Register 0, BIT 1) */
-	int ret_read, ret_write;
-	unsigned char cfg_val[1], out_val[1];
-	const void *blob = gd->fdt_blob;
-
-	/* Make sure board is supported (currently only A3700-DB is supported) */
-	if (fdt_node_check_compatible(blob, 0, "marvell,armada-3700-db0") != 0) {
-		error("Missing USB VBUS power configuration for current board.\n");
-		return;
-	}
-
-	printf("Enable USB VBUS.\n");
-
-	/* initialize I2C */
-	init_func_i2c();
-
-	/* Read configuration (direction) and set VBUS pin as output (reset pin = output) */
-	ret_read = i2c_read(I2C_IO_EXP_ADDR, I2C_IO_CFG_REG_0, sizeof(unsigned char), cfg_val, sizeof(cfg_val));
-	cfg_val[0] &= ~(1 << I2C_IO_REG_0_USB_H_OFF);
-	ret_write = i2c_write(I2C_IO_EXP_ADDR, I2C_IO_CFG_REG_0, sizeof(unsigned char),
-			cfg_val, sizeof(cfg_val));
-	if (ret_read || ret_write)
-		error("failed to set USB VBUS configuration on I2C IO expander\n");
-
-	/* Read VBUS output value, and disable it */
-	ret_read = i2c_read(I2C_IO_EXP_ADDR, I2C_IO_DATA_OUT_REG_0, sizeof(unsigned char), out_val, sizeof(out_val));
-	out_val[0] &= ~(1 << I2C_IO_REG_0_USB_H_OFF);
-
-	ret_write = i2c_write(I2C_IO_EXP_ADDR, I2C_IO_DATA_OUT_REG_0, sizeof(unsigned char), out_val, sizeof(out_val));
-	if (ret_read || ret_write)
-		error("failed to lower USB VBUS power on I2C IO expander\n");
-
-	/* required delay for configuration to settle - must wait for power on port is disabled
-	 * in case VBUS signal was high, required 3 seconds delay to let VBUS signal fully settle down */
-	udelay(3000000);
-
-	/* Enable VBUS power: Set output value of VBUS pin as enabled */
-	out_val[0] |= (1 << I2C_IO_REG_0_USB_H_OFF);
-
-	ret_write = i2c_write(I2C_IO_EXP_ADDR, I2C_IO_DATA_OUT_REG_0, sizeof(unsigned char), out_val, sizeof(out_val));
-	if (ret_write)
-		error("failed to raise USB VBUS power on I2C IO expander\n");
-
-	udelay(500000); /* required delay to let output value settle up*/
-
-
-}
-#endif /* CONFIG_USB_XHCI */
-
 /*
  * HW_INFO command is used to set board ID to EEPROM,
  * there will be no EEPROM Board ID if HW_INFO is disabled.

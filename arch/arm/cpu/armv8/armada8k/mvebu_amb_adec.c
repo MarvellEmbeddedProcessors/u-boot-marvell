b1fdf0c10c341c7eb0456c4d07cb310a172e83bd
Haim Boot <hayim@marvell.com>
2016-08-01 16:18:54 +0300

    amb: mvebu: add additional info to dump function
    - added window size information
    - added attribute string, instead of hex value
    
    Change-Id: I8747bba1ef56c4714466f860bdac43a8ab33f6a1
    Signed-off-by: Haim Boot <hayim@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/31647
    Reviewed-by: Igal Liberman <igall@marvell.com>
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Hanna Hawa <hannah@marvell.com>

--- arch/arm/cpu/armv8/armada8k/mvebu_amb_adec.c
+++ arch/arm/cpu/armv8/armada8k/mvebu_amb_adec.c
@@ -35,17 +35,74 @@
 #define AMB_WIN_CR_OFFSET(base, win)	(base + 0x0 + (0x8 * win))
 #define AMB_ATTR_OFFSET			(8)
 #define AMB_ATTR_MASK			(0xFF)
+#define AMB_SIZE_OFFSET			16
+#define AMB_SIZE_MASK			0xFF
 
 #define AMB_WIN_BASE_OFFSET(base, win)	(base + 0x4 + (0x8 * win))
 #define AMB_BASE_OFFSET			(16)
 
+#define AMB_WIN_ALIGNMENT_64K		(0x10000)
+
 DECLARE_GLOBAL_DATA_PTR;
+
+enum amb_attribute_ids {
+	AMB_SPI0_CS0_ID = 0x1E,
+	AMB_SPI0_CS1_ID = 0x5E,
+	AMB_SPI0_CS2_ID = 0x9E,
+	AMB_SPI0_CS3_ID = 0xDE,
+	AMB_SPI1_CS0_ID = 0x1A,
+	AMB_SPI1_CS1_ID = 0x5A,
+	AMB_SPI1_CS2_ID = 0x9A,
+	AMB_SPI1_CS3_ID = 0xDA,
+	AMB_DEV_CS0_ID  = 0x3E,
+	AMB_DEV_CS1_ID  = 0x3D,
+	AMB_DEV_CS2_ID  = 0x3B,
+	AMB_DEV_CS3_ID  = 0x37,
+	AMB_BOOT_CS_ID  = 0x2f,
+	AMB_BOOT_ROM_ID = 0x1D,
+	AMB_MAX_TID,
+	INVALID_TID     = 0xFF
+};
+
+struct amb_attribute_name_map {
+	enum amb_attribute_ids attr_id;
+	char name[10];
+};
+
+struct amb_attribute_name_map amb_attribute_name_table[] = {
+	{AMB_SPI0_CS0_ID,   "SPI0_CS0"},
+	{AMB_SPI0_CS1_ID,   "SPI0_CS1"},
+	{AMB_SPI0_CS2_ID,   "SPI0_CS2"},
+	{AMB_SPI0_CS3_ID,   "SPI0_CS3"},
+	{AMB_SPI1_CS0_ID,   "SPI1_CS0"},
+	{AMB_SPI1_CS1_ID,   "SPI1_CS1"},
+	{AMB_SPI1_CS2_ID,   "SPI1_CS2"},
+	{AMB_SPI1_CS3_ID,   "SPI1_CS3"},
+	{AMB_DEV_CS0_ID,    "DEV_CS0 "},
+	{AMB_DEV_CS1_ID,    "DEV_CS1 "},
+	{AMB_DEV_CS2_ID,    "DEV_CS2 "},
+	{AMB_DEV_CS3_ID,    "DEV_CS3 "},
+	{AMB_BOOT_CS_ID,    "BOOT_CS "},
+	{AMB_BOOT_ROM_ID,   "BOOT_ROM"},
+	{INVALID_TID,       "INVALID "},
+};
+
+static char *amb_attribute_name_get(enum amb_attribute_ids attr_id)
+{
+	int i;
+
+	for (i = 0; i < AMB_MAX_TID; i++)
+		if (amb_attribute_name_table[i].attr_id == attr_id)
+			return amb_attribute_name_table[i].name;
+	return amb_attribute_name_get(INVALID_TID);
+}
 
 void dump_amb_adec(void)
 {
 	const void *blob = gd->fdt_blob;
 	void __iomem *amb_base;
 	u32 ctrl, base, win_id, attr, node;
+	uint32_t size, size_count;
 
 	/* Get address decoding node from the FDT blob */
 	node = fdt_node_offset_by_compatible(blob, -1, fdtdec_get_compatible(COMPAT_MVEBU_AMB_ADEC));
@@ -57,14 +114,16 @@ void dump_amb_adec(void)
 	amb_base = (void *)fdt_get_regs_offs(blob, node, "reg");
 
 	/* Dump all AMB windows */
-	printf("bank  attribute     base\n");
-	printf("------------------------------\n");
+	printf("bank  attribute   base          size\n");
+	printf("--------------------------------------------\n");
 	for (win_id = 0; win_id < AMB_MAX_WIN_ID; win_id++) {
 		ctrl = readl(AMB_WIN_CR_OFFSET(amb_base, win_id));
 		if (ctrl & WIN_ENABLE_BIT) {
 			base = readl(AMB_WIN_BASE_OFFSET(amb_base, win_id));
 			attr = (ctrl >> AMB_ATTR_OFFSET) & AMB_ATTR_MASK;
-			printf("amb   0x%04x        0x%08x\n", attr, base);
+			size_count = (ctrl >> AMB_SIZE_OFFSET) & AMB_SIZE_MASK;
+			size = (size_count + 1) * AMB_WIN_ALIGNMENT_64K;
+			printf("amb   %s    0x%08x    0x%08x\n", amb_attribute_name_get(attr), base, size);
 		}
 	}
 

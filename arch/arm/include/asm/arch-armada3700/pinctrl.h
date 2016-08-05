6de8c4c64ce13a93beaa63ad126ea847195d0f6c
Ken Ma <make@marvell.com>
2016-08-05 23:08:15 +0800

    pin-ctrl: a3700: add mpp function selections for A0 chip
    - Armada3700 A0 chip has different mpp definitions than Z1.
      - Changed the controls for selecting multiplexed GPIO pin
      signals to unbundle the controlled signals and offer more
      flexibility.
      - Added the following fields:
      GPIO2_23_FUNC_GPIO_EN (RD0018830h [14])
      GPIO2_22_FUNC_GPIO_EN (RD0018830h [13])
      GPIO2_21_FUNC_GPIO_EN (RD0018830h [12])
      GPIO2_20_FUNC_GPIO_EN (RD0018830h [11])
      GPIO2_5_FUNC_GPIO_EN (RD0018830h [10])
      GPIO2_4_FUNC_GPIO_EN (RD0018830h [9])
      GPIO2_3_FUNC_GPIO_EN (RD0018830h [5])
      SMI_GPIO_EN (RD0018830h [4])
      - Updated RGMII_GPIO_EN (RD0018830h [3]) from {0h: RGMII/
        MII/SMI; 1h: GPIO} to {0h: RGMII; 1h: GPIO}
      - Removed PTP_GPIO_EN (RD0018830h [5]) and PCIE1_GPIO_EN
        (RD0018830h [4]).
    - This patch wraps the Z1 mpp function definitions with
      CONFIG_A3700_Z_SUPPORT option.
    
    Change-Id: Iec5bc3d8afc82940d0dccb1f4f068f49da7c2717
    Signed-off-by: Ken Ma <make@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/31778
    Reviewed-by: Wilson Ding <dingwei@marvell.com>
    Tested-by: Wilson Ding <dingwei@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/31753

--- arch/arm/include/asm/arch-armada3700/pinctrl.h
+++ arch/arm/include/asm/arch-armada3700/pinctrl.h
@@ -39,6 +39,7 @@
 #define MAX_PINS_PER_BANK	36
 #define MAX_FUNC_PER_PIN	0x3
 
+#ifdef CONFIG_A3700_Z_SUPPORT
 #define PINCTL_BANK_CONF { \
 	/* north bridge */ \
 	{	/* bank name,		reg_base,	pin_cnt */ \
@@ -121,5 +122,90 @@
 		}, \
 	}, \
 }
+#else
+/* Settings for A0 soc */
+#define PINCTL_BANK_CONF { \
+	/* north bridge */ \
+	{	/* bank name,		reg_base,	pin_cnt */ \
+		"armada-3700-nb",	0,		36, \
+		{ \
+			/* mask		func_num	reg_values */ \
+			{BIT10,		2,		{0, BIT10}		},	/* pin 0 */ \
+			{BIT10,		2,		{0, BIT10}		},	/* pin 1 */ \
+			{BIT9,		2,		{0, BIT9}		},	/* pin 2 */ \
+			{BIT9,		2,		{0, BIT9}		},	/* pin 3 */ \
+			{BIT16,		2,		{0, BIT16}		},	/* pin 4 */ \
+			{0,		1,		{0}			},	/* pin 5 */ \
+			{BIT8,		2,		{0, BIT8}		},	/* pin 6 */ \
+			{BIT7,		2,		{0, BIT7}		},	/* pin 7 */ \
+			{BIT1,		2,		{0, BIT1}		},	/* pin 8 */ \
+			{BIT1 | BIT19,	3,		{0, BIT1, BIT1 | BIT19}	},	/* pin 9 */ \
+			{BIT1 | BIT19,	3,		{0, BIT1, BIT1 | BIT19}	},	/* pin 10 */ \
+			{BIT3 | BIT20,	3,		{0, BIT3, BIT3 | BIT20}	},	/* pin 11 */ \
+			{BIT4 | BIT21,	3,		{0, BIT4, BIT4 | BIT21}	},	/* pin 12 */ \
+			{BIT5 | BIT22,	3,		{0, BIT5, BIT5 | BIT22}	},	/* pin 13 */ \
+			{BIT6 | BIT23,	3,		{0, BIT6, BIT6 | BIT23}	},	/* pin 14 */ \
+			{BIT18,		2,		{0, BIT18}		},	/* pin 15 */ \
+			{BIT18,		2,		{0, BIT18}		},	/* pin 16 */ \
+			{BIT12,		2,		{0, BIT12}		},	/* pin 17 */ \
+			{BIT13 | BIT19,	3,		{0, BIT13, BIT19}	},	/* pin 18 */ \
+			{BIT14 | BIT19,	3,		{0, BIT14, BIT19}	},	/* pin 19 */ \
+			{BIT0,		2,		{0, BIT0}		},	/* pin 20 */ \
+			{BIT0,		2,		{0, BIT0}		},	/* pin 21 */ \
+			{BIT0,		2,		{0, BIT0}		},	/* pin 22 */ \
+			{BIT0,		2,		{0, BIT0}		},	/* pin 23 */ \
+			{BIT0,		2,		{0, BIT0}		},	/* pin 24 */ \
+			{BIT17,		2,		{0, BIT17}		},	/* pin 25 */ \
+			{BIT17,		2,		{0, BIT17}		},	/* pin 26 */ \
+			{BIT2,		2,		{0, BIT2}		},	/* pin 27 */ \
+			{BIT2,		2,		{0, BIT2}		},	/* pin 28 */ \
+			{BIT2,		2,		{0, BIT2}		},	/* pin 29 */ \
+			{BIT2,		2,		{0, BIT2}		},	/* pin 30 */ \
+			{BIT2,		2,		{0, BIT2}		},	/* pin 31 */ \
+			{BIT2,		2,		{0, BIT2}		},	/* pin 32 */ \
+			{BIT2,		2,		{0, BIT2}		},	/* pin 33 */ \
+			{BIT2,		2,		{0, BIT2}		},	/* pin 34 */ \
+			{BIT2,		2,		{0, BIT2}		},	/* pin 35 */ \
+		}, \
+	}, \
+	/* South bridge */ \
+	{	/* bank name,		reg_base,	pin_cnt */ \
+		"armada-3700-sb",	0,		30, \
+		{ \
+			/*mask		config_num	configs*/ \
+			{BIT0,		2,		{0, BIT0}		},	/* pin 0 */ \
+			{BIT1,		2,		{0, BIT1}		},	/* pin 1 */ \
+			{0,		1,		{0}			},	/* pin 2 */ \
+			{BIT5,		2,		{0, BIT5}		},	/* pin 3 */ \
+			{BIT9,		2,		{0, BIT9}		},	/* pin 4 */ \
+			{BIT10,		2,		{0, BIT10}		},	/* pin 5 */ \
+			{BIT3,		2,		{0, BIT3}		},	/* pin 6 */ \
+			{BIT3,		2,		{0, BIT3}		},	/* pin 7 */ \
+			{BIT3,		2,		{0, BIT3}		},	/* pin 8 */ \
+			{BIT3,		2,		{0, BIT3}		},	/* pin 9 */ \
+			{BIT3,		2,		{0, BIT3}		},	/* pin 10 */ \
+			{BIT3,		2,		{0, BIT3}		},	/* pin 11 */ \
+			{BIT3,		2,		{0, BIT3}		},	/* pin 12 */ \
+			{BIT3,		2,		{0, BIT3}		},	/* pin 13 */ \
+			{BIT3,		2,		{0, BIT3}		},	/* pin 14 */ \
+			{BIT3,		2,		{0, BIT3}		},	/* pin 15 */ \
+			{BIT3,		2,		{0, BIT3}		},	/* pin 16 */ \
+			{BIT3,		2,		{0, BIT3}		},	/* pin 17 */ \
+			{BIT4,		2,		{0, BIT4}		},	/* pin 18 */ \
+			{BIT4,		2,		{0, BIT4}		},	/* pin 19 */ \
+			{BIT11,		2,		{0, BIT11}		},	/* pin 20 */ \
+			{BIT12 | BIT6,	3,		{0, BIT12, BIT6}	},	/* pin 21 */ \
+			{BIT13 | BIT7,	3,		{0, BIT13, BIT7}	},	/* pin 22 */ \
+			{BIT14 | BIT8,	3,		{0, BIT14, BIT8}	},	/* pin 23 */ \
+			{BIT2,		2,		{0, BIT2}		},	/* pin 24 */ \
+			{BIT2,		2,		{0, BIT2}		},	/* pin 25 */ \
+			{BIT2,		2,		{0, BIT2}		},	/* pin 26 */ \
+			{BIT2,		2,		{0, BIT2}		},	/* pin 27 */ \
+			{BIT2,		2,		{0, BIT2}		},	/* pin 28 */ \
+			{BIT2,		2,		{0, BIT2}		},	/* pin 29 */ \
+		}, \
+	}, \
+}
+#endif
 
 #endif /* _ARMADA3700_PINCTRL_H_ */

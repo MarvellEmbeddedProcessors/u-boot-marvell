5db192deec7aeb9c306e2e798bf014b7b0183213
Evan Wang <xswang@marvell.com>
2016-11-24 21:21:55 +0800

    comphy: mvebu: add comphy type PHY_TYPE_USB3
    - For some Marvell SoCs, like armada-3700, there are both
      USB host and device controller, but on PHY level the
      configuration is the same.
    - The new type supports both USB device and USB host
    
    Change-Id: Ia07448fd6d7f7f940cada5a1c5d366ebb325e24b
    Signed-off-by: Evan Wang <xswang@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/34031
    Reviewed-by: Igal Liberman <igall@marvell.com>
    Reviewed-by: Hua Jing <jinghua@marvell.com>
    Tested-by: Star_Automation <star@marvell.com>

--- drivers/phy/comphy_core.c
+++ drivers/phy/comphy_core.c
@@ -69,9 +69,10 @@ static char *get_type_string(u32 type)
 {
 	char *type_strings[] = {"UNCONNECTED", "PEX0", "PEX1", "PEX2", "PEX3",
 				"SATA0", "SATA1", "SATA2", "SATA3", "SGMII0",
-				"SGMII1", "SGMII2", "SGMII3", "QSGMII", "USB3_HOST0",
-				"USB3_HOST1", "USB3_DEVICE", "XAUI0", "XAUI1",
-				"XAUI2", "XAUI3", "RXAUI0", "RXAUI1", "KR", "IGNORE"};
+				"SGMII1", "SGMII2", "SGMII3", "QSGMII", "USB3",
+				"USB3_HOST0", "USB3_HOST1", "USB3_DEVICE", "XAUI0",
+				"XAUI1", "XAUI2", "XAUI3", "RXAUI0", "RXAUI1", "KR",
+				"IGNORE"};
 	if (type < 0 || type > PHY_TYPE_MAX)
 		return "invalid";
 	return type_strings[type];

6576a3c7fa905825052566d45ad26df4cf841fa5
Ken Ma <make@marvell.com>
2017-01-04 00:09:11 +0800

    scsi: move base, max_lun and max_id to uclass plat data
    - The members in scsi_platdata(base, max_lun and max_id) are generic,
      so now they are taken from fdt by the uclass_platdata instead of
      platdata code upon call to post bind callback.
    
    Change-Id: Ie211bbdd8c697831d303ebcb565a2155a05971fc
    Signed-off-by: Ken Ma <make@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/35304
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Omri Itach <omrii@marvell.com>
    Reviewed-by: Kostya Porotchkin <kostap@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/36496
    Reviewed-by: Igal Liberman <igall@marvell.com>

--- drivers/block/scsi-uclass.c
+++ drivers/block/scsi-uclass.c
@@ -11,7 +11,10 @@
 
 #include <common.h>
 #include <dm.h>
+#include <dm/device-internal.h>
 #include <scsi.h>
+
+DECLARE_GLOBAL_DATA_PTR;
 
 static int scsi_post_probe(struct udevice *dev)
 {
@@ -20,8 +23,34 @@ static int scsi_post_probe(struct udevice *dev)
 	return 0;
 }
 
+static void scsi_ofdata_to_uclass_platdata(struct udevice *dev)
+{
+	struct scsi_platdata *plat = dev_get_uclass_platdata(dev);
+	const void *blob = gd->fdt_blob;
+	int node = dev->of_offset;
+
+	plat->base = (unsigned long)dev_get_addr_ptr(dev);
+	plat->max_id = fdtdec_get_uint(blob,
+				       node,
+				       "max-id",
+				       CONFIG_SYS_SCSI_MAX_SCSI_ID);
+	plat->max_lun = fdtdec_get_uint(blob,
+					node,
+					"max-lun",
+					CONFIG_SYS_SCSI_MAX_LUN);
+	return;
+}
+
+static int scsi_post_bind(struct udevice *dev)
+{
+	/* Get uclass plat data from fdt */
+	scsi_ofdata_to_uclass_platdata(dev);
+}
+
 UCLASS_DRIVER(scsi) = {
 	.id		= UCLASS_SCSI,
 	.name		= "scsi",
+	.post_bind	= scsi_post_bind,
 	.post_probe	 = scsi_post_probe,
+	.per_device_platdata_auto_alloc_size = sizeof(struct scsi_platdata),
 };

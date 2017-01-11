d88a5765b9cc31becd78d9795834bda880d36620
Hanna Hawa <hannah@marvell.com>
2017-01-11 18:07:24 +0200

    fix: iob: cp110: fix display of IOB windows in CP110
    This patch fix IOB dump to print IOB windows for CP110-0, and CP110-1
    
    Change-Id: Ie2dabb8fc7ac33f80d406b29622069d15e599683
    Signed-off-by: Hanna Hawa <hannah@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/35506
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Haim Boot <hayim@marvell.com>
    Reviewed-by: Igal Liberman <igall@marvell.com>

--- arch/arm/cpu/armv8/armada8k/mvebu_iob.c
+++ arch/arm/cpu/armv8/armada8k/mvebu_iob.c
@@ -24,6 +24,8 @@
 #include <asm/arch-mvebu/fdt.h>
 #include <asm/arch-mvebu/iob.h>
 
+#define IOB_MAX_UNIT			2
+
 /* common defines */
 #define WIN_ENABLE_BIT			(0x1)
 /* Physical address of the base of the window = {AddrLow[19:0],20`h0} */
@@ -55,50 +57,55 @@ enum target_ids_iob {
 
 void dump_iob(void)
 {
-	u32 win_id, win_cr, alr, ahr;
+	u32 win_id, win_cr, alr, ahr, max_win;
 	u8 target_id;
 	u64 start, end;
 	const void *blob = gd->fdt_blob;
 	void __iomem *iob_base;
-	u32 max_win, node;
+	int node, node_list[IOB_MAX_UNIT], count, i;
 	char *iob_target_name[IOB_MAX_TID] = {"CONFIG", "IHB0  ", "PEX1  ", "PEX2  ",
 					      "PEX0  ", "NAND  ", "RUNIT ", "IHB1  "};
 
-	/* Get address decoding node from the FDT blob */
-	node = fdt_node_offset_by_compatible(blob, -1, fdtdec_get_compatible(COMPAT_MVEBU_IOB));
-	if (node < 0) {
-		error("No IOB address decoding node found in FDT blob\n");
-		return;
-	}
-	/* Get the base address of the address decoding MBUS */
-	iob_base = (void *)fdt_get_regs_offs(blob, node, "reg");
+	count = fdtdec_find_aliases_for_id(blob, "iob",
+			COMPAT_MVEBU_IOB, node_list, IOB_MAX_UNIT);
 
-	/* Get the maximum number of iob windows supported */
-	max_win = fdtdec_get_int(blob, node, "max-win", 0);
-	if (max_win == 0) {
-		max_win = IOB_MAX_WIN_NUM;
-		error("failed reading max windows number\n");
-	}
+	for (i = 0; i < count ; i++) {
+		node = node_list[i];
 
-	/* Dump all IOB windows */
-	printf("bank  id target  start		    end\n");
-	printf("----------------------------------------------------\n");
-	for (win_id = 0; win_id < max_win; win_id++) {
-		win_cr = readl(IOB_WIN_CR_OFFSET(iob_base, win_id));
-		if (win_cr & WIN_ENABLE_BIT) {
-			target_id = (win_cr >> IOB_TARGET_ID_OFFSET) & IOB_TARGET_ID_MASK;
-			alr = readl(IOB_WIN_ALR_OFFSET(iob_base, win_id));
-			start = ((u64)alr << ADDRESS_SHIFT);
-			if (win_id != 0) {
-				ahr = readl(IOB_WIN_AHR_OFFSET(iob_base, win_id));
-				end = (((u64)ahr + 0x10) << ADDRESS_SHIFT);
-			} else {
-				/* Window #0 size is hardcoded to 16MB, as it's
-				** reserved for CP configuration space. */
-				end = start + (16 << 20);
+		if (node <= 0)
+			continue;
+
+		/* Get the base address of the address decoding MBUS */
+		iob_base = (void *)fdt_get_regs_offs(blob, node, "reg");
+
+		/* Get the maximum number of iob windows supported */
+		max_win = fdtdec_get_int(blob, node, "max-win", 0);
+		if (max_win == 0) {
+			max_win = IOB_MAX_WIN_NUM;
+			error("failed reading max windows number\n");
+		}
+
+		/* Dump all IOB windows */
+		printf("bank  id target  start		    end\n");
+		printf("----------------------------------------------------\n");
+		for (win_id = 0; win_id < max_win; win_id++) {
+			win_cr = readl(IOB_WIN_CR_OFFSET(iob_base, win_id));
+			if (win_cr & WIN_ENABLE_BIT) {
+				target_id = (win_cr >> IOB_TARGET_ID_OFFSET) & IOB_TARGET_ID_MASK;
+				alr = readl(IOB_WIN_ALR_OFFSET(iob_base, win_id));
+				start = ((u64)alr << ADDRESS_SHIFT);
+				if (win_id != 0) {
+					ahr = readl(IOB_WIN_AHR_OFFSET(iob_base, win_id));
+					end = (((u64)ahr + 0x10) << ADDRESS_SHIFT);
+				} else {
+					/* Window #0 size is hardcoded to 16MB, as it's
+					 * reserved for CP configuration space.
+					 */
+					end = start + (16 << 20);
+				}
+				printf("iob   %02d %s  0x%016llx 0x%016llx\n",
+				       win_id, iob_target_name[target_id], start, end);
 			}
-			printf("iob   %02d %s  0x%016llx 0x%016llx\n"
-					, win_id, iob_target_name[target_id], start, end);
 		}
 	}
 

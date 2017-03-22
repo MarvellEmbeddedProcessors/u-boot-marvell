35e07a12c71335754d719ce3de0ae452b4814222
Konstantin Porotchkin <kostap@marvell.com>
2017-03-22 16:09:22 +0200

    mvebu: a37xx: Add init for ESPRESSBin Topaz switch
    Implement the board-specific network init function for
    ESPRESSOBin community board, setting the on-board Topaz
    switch port to forward mode and allow network connection
    through any of the available Etherenet ports.
    
    Change-Id: I1714053209984619ef7ad4789aa40cc59011a4e1
    Signed-off-by: Konstantin Porotchkin <kostap@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/37840
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Igal Liberman <igall@marvell.com>
    Reviewed-by: Wilson Ding <dingwei@marvell.com>

--- board/mvebu/armada3700/armada3700.c
+++ board/mvebu/armada3700/armada3700.c
@@ -19,10 +19,108 @@
 /* #define DEBUG*/
 
 #include <common.h>
+#include <asm/io.h>
+#include <asm/errno.h>
+#include <phy.h>
+#include <fdtdec.h>
+#include <asm/arch-mvebu/fdt.h>
 #include <linux/compiler.h>
+
+/* Ethernet switch registers */
+/* SMI addresses for multi-chip mode */
+#define MVEBU_PORT_CTRL_SMI_ADDR(p)    (16 + (p))
+#define MVEBU_SW_G2_SMI_ADDR           (28)
+
+/* Multi-chip mode */
+#define MVEBU_SW_SMI_DATA_REG          (1)
+#define MVEBU_SW_SMI_CMD_REG           (0)
+ #define SW_SMI_CMD_REG_ADDR_OFF       0
+ #define SW_SMI_CMD_DEV_ADDR_OFF       5
+ #define SW_SMI_CMD_SMI_OP_OFF         10
+ #define SW_SMI_CMD_SMI_MODE_OFF       12
+ #define SW_SMI_CMD_SMI_BUSY_OFF       15
+
+/* Single-chip mode */
+/* Switch Port Registers */
+#define MVEBU_SW_LINK_CTRL_REG         (1)
+#define MVEBU_SW_PORT_CTRL_REG         (4)
+
+/* Global 2 Registers */
+#define MVEBU_G2_SMI_PHY_CMD_REG       (24)
+#define MVEBU_G2_SMI_PHY_DATA_REG      (25)
+
+DECLARE_GLOBAL_DATA_PTR;
 
 int mvebu_board_id_get(void)
 {
 	/* stub function for future use */
+	return 0;
+}
+
+/* Helper function for accessing switch devices in multi-chip connection mode */
+static int mii_multi_chip_mode_write(struct mii_dev *bus, int dev_smi_addr,
+				     int smi_addr, int reg, u16 value)
+{
+	u16 smi_cmd = 0;
+
+	if (bus->write(bus, dev_smi_addr, 0,
+		       MVEBU_SW_SMI_DATA_REG, value) != 0) {
+		printf("Error writing to the PHY addr=%02x reg=%02x\n",
+		       smi_addr, reg);
+		return -EFAULT;
+	}
+
+	smi_cmd = (1 << SW_SMI_CMD_SMI_BUSY_OFF) |
+		  (1 << SW_SMI_CMD_SMI_MODE_OFF) |
+		  (1 << SW_SMI_CMD_SMI_OP_OFF) |
+		  (smi_addr << SW_SMI_CMD_DEV_ADDR_OFF) |
+		  (reg << SW_SMI_CMD_REG_ADDR_OFF);
+	if (bus->write(bus, dev_smi_addr, 0,
+		       MVEBU_SW_SMI_CMD_REG, smi_cmd) != 0) {
+		printf("Error writing to the PHY addr=%02x reg=%02x\n",
+		       smi_addr, reg);
+		return -EFAULT;
+	}
+
+	return 0;
+}
+
+/* Bring-up board-specific network stuff */
+int board_network_enable(struct mii_dev *bus)
+{
+	/* Needed only for EspressoBin board */
+	if (fdt_node_offset_by_compatible(gd->fdt_blob, -1,
+					  "marvell,armada-3700-espressobin") < 0)
+		return 0;
+
+	/*
+	 * FIXME: remove this code once Topaz driver gets available
+	 * A3720 Community Board Only
+	 * Configure Topaz switch (88E6341)
+	 * Set port 0,1,2,3 to forwarding Mode (through Switch Port registers)
+	 */
+	mii_multi_chip_mode_write(bus, 1, MVEBU_PORT_CTRL_SMI_ADDR(0),
+				  MVEBU_SW_PORT_CTRL_REG, 0x7f);
+	mii_multi_chip_mode_write(bus, 1, MVEBU_PORT_CTRL_SMI_ADDR(1),
+				  MVEBU_SW_PORT_CTRL_REG, 0x7f);
+	mii_multi_chip_mode_write(bus, 1, MVEBU_PORT_CTRL_SMI_ADDR(2),
+				  MVEBU_SW_PORT_CTRL_REG, 0x7f);
+	mii_multi_chip_mode_write(bus, 1, MVEBU_PORT_CTRL_SMI_ADDR(3),
+				  MVEBU_SW_PORT_CTRL_REG, 0x7f);
+
+	/* RGMII Delay on Port 0 (CPU port), force link to 1000Mbps */
+	mii_multi_chip_mode_write(bus, 1, MVEBU_PORT_CTRL_SMI_ADDR(0),
+				  MVEBU_SW_LINK_CTRL_REG, 0xe002);
+
+	/* Power up PHY 1, 2, 3 (through Global 2 registers) */
+	mii_multi_chip_mode_write(bus, 1, MVEBU_SW_G2_SMI_ADDR,
+				  MVEBU_G2_SMI_PHY_DATA_REG, 0x1140);
+	mii_multi_chip_mode_write(bus, 1, MVEBU_SW_G2_SMI_ADDR,
+				  MVEBU_G2_SMI_PHY_CMD_REG, 0x9620);
+	mii_multi_chip_mode_write(bus, 1, MVEBU_SW_G2_SMI_ADDR,
+				  MVEBU_G2_SMI_PHY_CMD_REG, 0x9640);
+	mii_multi_chip_mode_write(bus, 1, MVEBU_SW_G2_SMI_ADDR,
+				  MVEBU_G2_SMI_PHY_CMD_REG, 0x9660);
+
 	return 0;
 }

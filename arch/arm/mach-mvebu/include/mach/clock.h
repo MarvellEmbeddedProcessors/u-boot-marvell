1d3fd804b90aa47c43b87ca68495edd8236f24ea
allen yan <yanwei@marvell.com>
2017-04-18 17:39:47 +0800

    clock: a3700: porting clock driver
    - Clock driver is used to print SoC information during
      boot, this patch porting clock driver from u-boot-2015
      to u-boot-2017.
    
    Change-Id: I2c5b3ed2a75c578aa89ff85c417c923845beb7a4
    Signed-off-by: allen yan <yanwei@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/38656
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Hua Jing <jinghua@marvell.com>

---
+++ arch/arm/mach-mvebu/include/mach/clock.h
@@ -0,0 +1,34 @@
+/*
+ * ***************************************************************************
+ * Copyright (C) 2015 Marvell International Ltd.
+ * ***************************************************************************
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 2 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * ***************************************************************************
+ */
+
+#ifndef _MVEBU_CLOCK_H_
+#define _MVEBU_CLOCK_H_
+
+#define KHz			1000
+#define MHz			1000000
+#define GHz			1000000000
+
+u32 soc_tclk_get(void);
+u32 soc_l2_clk_get(void);
+u32 soc_cpu_clk_get(void);
+u32 soc_ddr_clk_get(void);
+u32 soc_timer_clk_get(void);
+
+void soc_print_clock_info(void);
+
+#endif /* _MVEBU_CLOCK_H_ */

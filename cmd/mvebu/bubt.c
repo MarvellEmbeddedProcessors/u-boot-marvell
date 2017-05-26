8715b3657297ecb255a7a0a4479647b1ee4dc3b6
zachary <zhangzg@marvell.com>
2017-05-26 10:41:58 +0800

    fix: bubt: mvebu: check read image file return value correctly
    Bubt command used to get stuck when fails to get file from source dev.
    This issue happens when read routine returns a negative value on failure. But
    bubt processes it as a unsigned int. So it becomes a really big positive value
    and pass bubt check cretiria.
    The issue happened due to wrong image_size type (unsigned int) for criteria.
    This patch checks read function return value as int.
    
    Change-Id: I116c90725d65b19387122eb934b1e2fd60433938
    Signed-off-by: zachary <zhangzg@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/39565
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Igal Liberman <igall@marvell.com>

--- cmd/mvebu/bubt.c
+++ cmd/mvebu/bubt.c
@@ -430,9 +430,15 @@ static int is_usb_active(void)
 #ifdef CONFIG_CMD_NET
 static size_t tftp_read_file(const char *file_name)
 {
+	int rc;
+
 	/* update global variable load_addr before tftp file from network */
 	load_addr = get_load_addr();
-	return net_loop(TFTPGET);
+	rc = net_loop(TFTPGET);
+	if (rc <= 0)
+		return 0;
+	else
+		return rc;
 }
 
 static int is_tftp_active(void)

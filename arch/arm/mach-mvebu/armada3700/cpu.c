f42fa28803750e0529d7df479ec58b17e5e6e897
Ken Ma <make@marvell.com>
2017-07-26 10:35:10 +0800

    Revert "pcie: a3700: update pcie memory region and fdt range"
    This reverts 'commit f253ef658b41
    ("pcie: a3700: update pcie memory region and fdt range")'.
    
    The patch solves the backward compability issue prior to
    Armada3700 17.06 BSP release. The correlated patches of
    other BSP components (ATF & Linux) should be applied as
    well.
    
    Regarding 4GB memory support, please refer to the release
    notes.
    
    Change-Id: Ie531817f81bfefe14eb189dca6ba0cc1fc0fa300
    Signed-off-by: Ken Ma <make@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/42176
    Reviewed-by: Wilson Ding <dingwei@marvell.com>
    Tested-by: Wilson Ding <dingwei@marvell.com>

--- arch/arm/mach-mvebu/armada3700/cpu.c
+++ arch/arm/mach-mvebu/armada3700/cpu.c
@@ -49,8 +49,8 @@ static struct mm_region mvebu_mem_map[] = {
 	},
 	{
 		/* PCI regions */
-		.phys = 0xd8200000UL,
-		.virt = 0xd8200000UL,
+		.phys = 0xe8000000UL,
+		.virt = 0xe8000000UL,
 		.size = 0x02000000UL,	/* 32MiB master PCI space */
 		.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
 			 PTE_BLOCK_NON_SHARE

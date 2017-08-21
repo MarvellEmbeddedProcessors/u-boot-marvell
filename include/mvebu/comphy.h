464a77a972cf0b793164cb694d6faed9e95a238d
Igal Liberman <igall@marvell.com>
2017-08-21 17:03:23 +0300

    phy: marvell: add RX training command
    This patch adds support for running RX training using new command called
    "rx_training"
    Usage:
    rx_training - rx_training <cp id> <comphy id>
    
    RX training allows to improve link quality (for SFI mode)
    by running training sequence between us and the link partner,
    this allows to reach better link quality then using static configuration.
    
    Change-Id: If1fc12255c4eebf77c4c4e26ae0042decf4cdf51
    Signed-off-by: Igal Liberman <igall@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/41749
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Kostya Porotchkin <kostap@marvell.com>

---
+++ include/mvebu/comphy.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2017 Marvell International Ltd.
+ *
+ * SPDX-License-Identifier:	GPL-2.0
+ * https://spdx.org/licenses
+ */
+
+#ifndef _COMPHY_H_
+#define _COMPHY_H_
+
+int comphy_rx_training(struct udevice *dev, u32 lane);
+
+#endif /* _COMPHY_H_ */

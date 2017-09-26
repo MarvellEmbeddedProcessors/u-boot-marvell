30d841ab5f340b758d7be8b8aa5fdd29aecf8a16
Hanna Hawa <hannah@marvell.com>
2017-09-26 17:54:00 +0300

    mvebu: armada-8k: add regulator definition in generic regulator usage
    Change-Id: If69e33ea340e861fddb34f2864c643043c065033
    Signed-off-by: Hanna Hawa <hannah@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/44701
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Hua Jing <jinghua@marvell.com>
    Reviewed-by: Kostya Porotchkin <kostap@marvell.com>
    Reviewed-by: Igal Liberman <igall@marvell.com>

--- arch/arm/mach-mvebu/arm64-common.c
+++ arch/arm/mach-mvebu/arm64-common.c
@@ -142,9 +142,11 @@ int arch_early_init_r(void)
 	int ret;
 	int i;
 
+#ifdef CONFIG_DM_REGULATOR
 	/* Check if any existing regulator should be turned down */
 	if (!of_machine_is_compatible("marvell,armada3710"))
 		regulators_enable_boot_off(false);
+#endif
 
 	/*
 	 * Loop over all MISC uclass drivers to call the comphy code

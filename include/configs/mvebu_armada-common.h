c21cb6d1621628f4bc4f666ad3f497b1f7dad17b
Andre Heider <a.heider@gmail.com>
2017-12-01 07:53:31 +0100

    arm: mvebu: configs: use distro compatible 'ramdisk_addr_r'

--- include/configs/mvebu_armada-common.h
+++ include/configs/mvebu_armada-common.h
@@ -42,7 +42,7 @@
 					"loadaddr=0x5000000\0"		\
 					"fdt_high=0xffffffffffffffff\0"	\
 					"hostname=marvell\0"		\
-					"ramfs_addr=0x8000000\0"	\
+					"ramdisk_addr_r=0x8000000\0"	\
 					"ramfs_name=-\0"		\
 					"fdt_name=fdt.dtb\0"		\
 					"netdev=eth0\0"			\
@@ -53,10 +53,10 @@
 					"image_name=Image\0"		\
 					"get_ramfs=if test \"${ramfs_name}\"" \
 						" != \"-\"; then setenv " \
-						"ramfs_addr 0x8000000; " \
+						"ramdisk_addr_r 0x8000000; " \
 						"tftpboot $ramfs_addr " \
 						"$ramfs_name; else setenv " \
-						"ramfs_addr -;fi\0"	\
+						"ramdisk_addr_r -;fi\0"	\
 					"get_images=tftpboot $kernel_addr " \
 						"$image_name; tftpboot " \
 						"$fdt_addr_r $fdt_name; " \
@@ -70,7 +70,7 @@
 						"$serverip:$rootpath " \
 						"$extra_params"
 #define CONFIG_BOOTCOMMAND	"run get_images; run set_bootargs; " \
-				"booti $kernel_addr $ramfs_addr $fdt_addr_r"
+				"booti $kernel_addr $ramdisk_addr_r $fdt_addr_r"
 #define CONFIG_ENV_OVERWRITE	/* ethaddr can be reprogrammed */
 /*
  * For booting Linux, the board info and command line data

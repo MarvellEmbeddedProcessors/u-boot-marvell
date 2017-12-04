c54a8d0652548b44dd021d13ab923b357168a5e4
Andre Heider <a.heider@gmail.com>
2017-12-04 07:36:19 +0100

    arm: mvebu: configs: use distro defaults
    Change-Id: Ib0ea5e203843dc0d793b9bdabbb209f90fb2105f
    Signed-off-by: Andre Heider <a.heider@gmail.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/47973
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Igal Liberman <igall@marvell.com>

--- include/configs/mvebu_armada-common.h
+++ include/configs/mvebu_armada-common.h
@@ -96,9 +96,6 @@
 /*
  * Other required minimal configurations
  */
-#define CONFIG_SYS_LONGHELP
-#define CONFIG_AUTO_COMPLETE
-#define CONFIG_CMDLINE_EDITING
 #define CONFIG_ARCH_CPU_INIT		/* call arch_cpu_init() */
 #define CONFIG_SYS_LOAD_ADDR	0x00800000	/* default load adr- 8M */
 #define CONFIG_SYS_MEMTEST_START 0x00800000	/* 8M */
@@ -230,5 +227,7 @@
 					"$ramfs_addr $fdt_addr"
 
 #endif /*CONFIG_MVEBU_PALLADIUM*/
+
+#include <config_distro_defaults.h>
 
 #endif /* _CONFIG_MVEBU_ARMADA_H */

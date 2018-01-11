e8aa628f402edfc12354c7d5195e604aba1dbb3d
zachary zhang <zhangzg@marvell.com>
2018-01-11 15:58:25 +0800

    fix: env: use chip block size for spi-nand saveenv
    When saving env to spi-nand, the erase or write operation
    must be block-size aligned. The CONFIG_ENV_SECT_SIZE is
    defined in compile-time but it could be different for
    various spi-nand chips. Therefore, the chip block size is
    preferred to be used for saveenv command.
    
    Change-Id: Ic1d41c70dd53606955addca8c1bb628dd44ecf3f
    Signed-off-by: zachary zhang <zhangzg@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/48827
    Reviewed-by: Wilson Ding <dingwei@marvell.com>
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Igal Liberman <igall@marvell.com>

--- common/env_spi_nand.c
+++ common/env_spi_nand.c
@@ -55,7 +55,7 @@ int saveenv(void)
 {
 	env_t	env_new;
 	char	*saved_buffer = NULL, flag = OBSOLETE_FLAG;
-	u32	saved_size, saved_offset, sector = 1;
+	u32	saved_size = 0, saved_offset, sector = 1;
 	int	ret;
 
 	if (!env_flash) {
@@ -82,8 +82,8 @@ int saveenv(void)
 	}
 
 	/* Is the sector larger than the env (i.e. embedded) */
-	if (CONFIG_ENV_SECT_SIZE > CONFIG_ENV_SIZE) {
-		saved_size = CONFIG_ENV_SECT_SIZE - CONFIG_ENV_SIZE;
+	if (env_flash->block_size > CONFIG_ENV_SIZE) {
+		saved_size = env_flash->block_size - CONFIG_ENV_SIZE;
 		saved_offset = env_new_offset + CONFIG_ENV_SIZE;
 		saved_buffer = malloc(saved_size);
 		if (!saved_buffer) {
@@ -96,15 +96,15 @@ int saveenv(void)
 			goto done;
 	}
 
-	if (CONFIG_ENV_SIZE > CONFIG_ENV_SECT_SIZE) {
-		sector = CONFIG_ENV_SIZE / CONFIG_ENV_SECT_SIZE;
-		if (CONFIG_ENV_SIZE % CONFIG_ENV_SECT_SIZE)
+	if (CONFIG_ENV_SIZE > env_flash->block_size) {
+		sector = CONFIG_ENV_SIZE / env_flash->block_size;
+		if (CONFIG_ENV_SIZE % env_flash->block_size)
 			sector++;
 	}
 
 	puts("Erasing SPI NAND flash...");
 	ret = spi_nand_cmd_erase_ops(env_flash, env_new_offset,
-			sector * CONFIG_ENV_SECT_SIZE, true);
+			sector * env_flash->block_size, true);
 	if (ret)
 		goto done;
 
@@ -115,7 +115,7 @@ int saveenv(void)
 	if (ret)
 		goto done;
 
-	if (CONFIG_ENV_SECT_SIZE > CONFIG_ENV_SIZE) {
+	if (env_flash->block_size > CONFIG_ENV_SIZE) {
 		ret = spi_nand_cmd_write_ops(env_flash, saved_offset,
 					saved_size, saved_buffer);
 		if (ret)
@@ -236,7 +236,7 @@ static int spi_nand_saveenv(void)
 int saveenv(void)
 #endif
 {
-	u32	saved_size, saved_offset, sector = 1;
+	u32	saved_size = 0, saved_offset, sector = 1;
 	char	*saved_buffer = NULL;
 	int	ret = 1;
 	env_t	env_new;
@@ -252,8 +252,8 @@ int saveenv(void)
 	}
 
 	/* Is the sector larger than the env (i.e. embedded) */
-	if (CONFIG_ENV_SECT_SIZE > CONFIG_ENV_SIZE) {
-		saved_size = CONFIG_ENV_SECT_SIZE - CONFIG_ENV_SIZE;
+	if (env_flash->block_size > CONFIG_ENV_SIZE) {
+		saved_size = env_flash->block_size - CONFIG_ENV_SIZE;
 		saved_offset = CONFIG_ENV_OFFSET + CONFIG_ENV_SIZE;
 		saved_buffer = malloc(saved_size);
 		if (!saved_buffer)
@@ -265,9 +265,9 @@ int saveenv(void)
 			goto done;
 	}
 
-	if (CONFIG_ENV_SIZE > CONFIG_ENV_SECT_SIZE) {
-		sector = CONFIG_ENV_SIZE / CONFIG_ENV_SECT_SIZE;
-		if (CONFIG_ENV_SIZE % CONFIG_ENV_SECT_SIZE)
+	if (CONFIG_ENV_SIZE > env_flash->block_size) {
+		sector = CONFIG_ENV_SIZE / env_flash->block_size;
+		if (CONFIG_ENV_SIZE % env_flash->block_size)
 			sector++;
 	}
 
@@ -277,7 +277,7 @@ int saveenv(void)
 
 	puts("Erasing SPI NAND flash...");
 	ret = spi_nand_cmd_erase_ops(env_flash, CONFIG_ENV_OFFSET,
-		sector * CONFIG_ENV_SECT_SIZE, true);
+		sector * env_flash->block_size, true);
 	if (ret)
 		goto done;
 
@@ -287,7 +287,7 @@ int saveenv(void)
 	if (ret)
 		goto done;
 
-	if (CONFIG_ENV_SECT_SIZE > CONFIG_ENV_SIZE) {
+	if (env_flash->block_size > CONFIG_ENV_SIZE) {
 		ret = spi_nand_cmd_write_ops(env_flash, saved_offset,
 			saved_size, saved_buffer);
 		if (ret)

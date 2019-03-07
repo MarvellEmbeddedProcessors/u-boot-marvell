cfd6ee4f8489d12a298675472394c58be0b8feda
Tomasz Michalec <tomasz@marvell.com>
2019-03-07 19:54:56 +0100

    octeontx2: cgx: Implement command to call SERDES diagnostic commands in ATF
    Change-Id: I8d5efc979438b4bdc56540f3250cd89823456ab4
    Signed-off-by: Tomasz Michalec <tomasz@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/11186
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Suneel Garapati <sgarapati@caviumnetworks.com>

---
+++ cmd/marvell/octeontx2_qlm_cmds.c
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier:    GPL-2.0
+/*
+ * https://spdx.org/licenses
+ *
+ * Copyright (C) 2019 Marvell International Ltd.
+ */
+
+#include <common.h>
+#include <command.h>
+#include <asm/arch/atf.h>
+
+extern int cgx_intf_prbs(u8 qlm, u8 mode, u32 time);
+extern int cgx_intf_display_eye(u8 qlm, u8 lane);
+
+static int do_prbs(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	ulong time;
+	ulong mode;
+	ulong qlm;
+
+	if (argc != 4)
+		return CMD_RET_USAGE;
+
+	qlm = simple_strtoul(argv[1], NULL, 10);
+	mode = simple_strtoul(argv[2], NULL, 10);
+	time = simple_strtoul(argv[3], NULL, 10);
+
+	cgx_intf_prbs(qlm, mode, time);
+
+	return CMD_RET_SUCCESS;
+}
+
+U_BOOT_CMD(prbs,    4,    1,     do_prbs,
+	   "command to run PRBS on slected QLM",
+	   "<qlm> <mode> <time>\n"
+	   "    - run PRBS with pattern indicated by 'mode' on selected 'qlm'\n"
+	   "      PRBS will be enabled by 'time' seconds"
+);
+
+static int do_eye(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	ulong lane;
+	ulong qlm;
+
+	if (argc != 3)
+		return CMD_RET_USAGE;
+
+	qlm = simple_strtoul(argv[1], NULL, 10);
+	lane = simple_strtoul(argv[2], NULL, 10);
+
+	cgx_intf_display_eye(qlm, lane);
+
+	return CMD_RET_SUCCESS;
+}
+
+U_BOOT_CMD(eye,    3,    1,     do_eye,
+	   "command to display eye on slected QLM",
+	   "<qlm> <lane>\n"
+	   "    - run eye by 'lane'  'qlm'\n"
+);
+

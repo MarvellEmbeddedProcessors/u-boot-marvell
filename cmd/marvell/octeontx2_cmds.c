12f0f6217c5171b61ae72956fd6e1c423cd4079b
Suneel Garapati <sgarapati@marvell.com>
2019-05-13 15:53:34 -0700

    cmd: marvell: fix compile and kconfig errors
    Change-Id: I7c3816d9d1c869b3fbd8ddcd278243f97a60a748
    Signed-off-by: Suneel Garapati <sgarapati@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/8934
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Suneel Garapati <sgarapati@caviumnetworks.com>

--- cmd/marvell/octeontx2_cmds.c
+++ cmd/marvell/octeontx2_cmds.c
@@ -9,7 +9,7 @@
 #include <command.h>
 #include <asm/arch/smc.h>
 
-static int do_octeontx2_flsf(
+static int mrvl_otx2_fsafe_clr_sec_boot(
 	cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[])
 {
 	return smc_flsf_clr_force_2ndry();

6d27ed567d9a976f6386c6a807221519a24ad5c8
Suneel Garapati <sgarapati@marvell.com>
2019-07-28 20:38:00 -0700

    cmd: marvell: Add mdio_dbg command support
    mdio_dbg command supports read and write of PHY registers
    on Marvell OcteonTX2 RVU PF interfaces with valid PHY
    interface. Actual operations over SMI bus are achieved
    using SMC calls to ATF.
    
    Uncomment CMD_MDIO_DBG in include/configs/octeontx2_<platform>.h
    file to enable this command.
    
    Change-Id: I99d23c51eb9cf29a0e683264c3211d67ada477a4
    Signed-off-by: Suneel Garapati <sgarapati@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/13204
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>

--- arch/arm/include/asm/arch-octeontx2/smc.h
+++ arch/arm/include/asm/arch-octeontx2/smc.h
@@ -15,5 +15,9 @@ ssize_t smc_dram_size(unsigned int node);
 ssize_t smc_disable_rvu_lfs(unsigned int node);
 ssize_t smc_flsf_fw_booted(void);
 ssize_t smc_flsf_clr_force_2ndry(void);
+ssize_t smc_mdio_dbg_read(int cgx_lmac, int mode, int phyaddr, int devad,
+			  int reg);
+ssize_t smc_mdio_dbg_write(int cgx_lmac, int mode, int phyaddr, int devad,
+			   int reg, int val);
 
 #endif
--- board/Marvell/octeontx2/smc.c
+++ board/Marvell/octeontx2/smc.c
@@ -66,3 +66,36 @@ ssize_t smc_flsf_clr_force_2ndry(void)
 
 	return regs.regs[0];
 }
+
+ssize_t smc_mdio_dbg_read(int cgx_lmac, int mode, int phyaddr, int devad,
+			  int reg)
+{
+	struct pt_regs regs;
+
+	regs.regs[0] = OCTEONTX2_MDIO_DBG_READ;
+	regs.regs[1] = cgx_lmac;
+	regs.regs[2] = mode;
+	regs.regs[3] = phyaddr;
+	regs.regs[4] = devad;
+	regs.regs[5] = reg;
+	smc_call(&regs);
+
+	return regs.regs[0];
+}
+
+ssize_t smc_mdio_dbg_write(int cgx_lmac, int mode, int phyaddr, int devad,
+			   int reg, int val)
+{
+	struct pt_regs regs;
+
+	regs.regs[0] = OCTEONTX2_MDIO_DBG_WRITE;
+	regs.regs[1] = cgx_lmac;
+	regs.regs[2] = mode;
+	regs.regs[3] = phyaddr;
+	regs.regs[4] = devad;
+	regs.regs[5] = reg;
+	regs.regs[6] = val;
+	smc_call(&regs);
+
+	return regs.regs[0];
+}

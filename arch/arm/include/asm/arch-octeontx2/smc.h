f175f3c75b05984293773c2724a965d1c3a7941c
Rick Farrington <Ricardo.Farrington@cavium.com>
2019-10-28 15:10:22 -0400

    smc: attest: add conditional guard around SMC attestation code
    Change-Id: Ie5a4560178098256e6a4a6c0604193f557d1efe3
    Signed-off-by: Rick Farrington <Ricardo.Farrington@cavium.com>
    Reviewed-on: https://sj1git1.cavium.com/17948
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Derek Chickles <Derek.Chickles@cavium.com>
    Reviewed-by: Aaron Williams <awilliams@marvell.com>

--- arch/arm/include/asm/arch-octeontx2/smc.h
+++ arch/arm/include/asm/arch-octeontx2/smc.h
@@ -19,6 +19,8 @@ ssize_t smc_mdio_dbg_read(int cgx_lmac, int mode, int phyaddr, int devad,
 			  int reg);
 ssize_t smc_mdio_dbg_write(int cgx_lmac, int mode, int phyaddr, int devad,
 			   int reg, int val);
+#ifdef CONFIG_CMD_ATTEST
 ssize_t smc_attest(long subcmd, long ctx_arg);
+#endif
 
 #endif

6fc412faa4b7ce65d7d9e524ceb6c87a648f255a
selvam venkatachalam <selvamv@marvell.com>
2020-07-09 17:12:09 -0400

    U-boot will use SMC calls for SERDES diags
    with this change uboot will use the SMC call for SERDES diags.
    support for cgx calls to do SERDES diags is removed.
    
    Change-Id: If8e52a125fbd3335bc4f4f49cc50c31f5c4ecb75
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/u-boot/+/31717
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>

--- cmd/marvell/octeontx2_qlm_cmds.c
+++ cmd/marvell/octeontx2_qlm_cmds.c
@@ -4,20 +4,39 @@
  *
  * Copyright (C) 2019 Marvell International Ltd.
  */
-
+#include <asm/psci.h>
 #include <common.h>
 #include <command.h>
+#include <linux/arm-smccc.h>
+#include <linux/compiler.h>
+#include <linux/psci.h>
+#include <asm/arch/smc.h>
 
-extern int cgx_intf_prbs(u8 qlm, u8 mode, u32 time, u32 lane);
-extern int cgx_intf_display_eye(u8 qlm, u8 lane);
-extern int cgx_intf_display_serdes(u8 qlm, u8 lane);
+enum cgx_prbs_cmd {
+        CGX_PRBS_START_CMD = 1,
+        CGX_PRBS_STOP_CMD,
+        CGX_PRBS_GET_DATA_CMD
+};
+
+#define SMCCC_RET_PENDING 1
+
+static void waitforresult(struct arm_smccc_res *res)
+{
+	int fdelay = 1000;
+	while (fdelay && (res->a0 == SMCCC_RET_PENDING)) {
+		mdelay(100);
+		fdelay = fdelay - 100;
+	}
+}
 
 static int do_prbs(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
-	ulong time;
+	struct arm_smccc_res res;
+	long time;
 	ulong mode;
 	ulong qlm;
 	ulong lane;
+	ulong delay;
 
 	if (argc == 5) {
 		qlm = simple_strtoul(argv[1], NULL, 10);
@@ -28,8 +47,38 @@ static int do_prbs(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 		return CMD_RET_USAGE;
 	}
 
-	cgx_intf_prbs(qlm, mode, time, lane);
+	arm_smccc_smc(OCTEONTX_SERDES_DBG_PRBS, CGX_PRBS_START_CMD, qlm, mode, lane, 0, 0, 0, &res);
 
+	waitforresult(&res);
+	if (res.a0 != SMCCC_RET_SUCCESS) {
+		return CMD_RET_FAILURE;
+	}
+
+	while(time > 0)
+	{
+		if(time > 5) {
+			delay = 5;
+			time = time - 5;
+		}
+		else {
+			delay = time;
+			time  = 0;
+		}
+		mdelay(delay * 1000);
+		arm_smccc_smc(OCTEONTX_SERDES_DBG_PRBS, CGX_PRBS_GET_DATA_CMD, qlm, 1, lane, 0, 0, 0, &res);
+		waitforresult(&res);
+
+		if (res.a0 != SMCCC_RET_SUCCESS) {
+			return CMD_RET_FAILURE;
+		}
+	}
+
+	arm_smccc_smc(OCTEONTX_SERDES_DBG_PRBS, CGX_PRBS_STOP_CMD, qlm, mode, lane, 0, 0, 0, &res);
+	waitforresult(&res);
+
+	if (res.a0 != SMCCC_RET_SUCCESS) {
+		return CMD_RET_FAILURE;
+	}
 	return CMD_RET_SUCCESS;
 }
 
@@ -43,6 +92,7 @@ U_BOOT_CMD(prbs,    5,    1,     do_prbs,
 
 static int do_eye(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
+	struct arm_smccc_res res;
 	ulong lane;
 	ulong qlm;
 
@@ -52,8 +102,12 @@ static int do_eye(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	qlm = simple_strtoul(argv[1], NULL, 10);
 	lane = simple_strtoul(argv[2], NULL, 10);
 
-	cgx_intf_display_eye(qlm, lane);
+	arm_smccc_smc(OCTEONTX_SERDES_DBG_GET_EYE, qlm, lane, 1, 0, 0, 0, 0, &res);
+	waitforresult(&res);
 
+        if (res.a0 != SMCCC_RET_SUCCESS) {
+                return CMD_RET_FAILURE;
+        }
 	return CMD_RET_SUCCESS;
 }
 
@@ -65,6 +119,7 @@ U_BOOT_CMD(eye,    3,    1,     do_eye,
 
 static int do_serdes(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
+	struct arm_smccc_res res;
 	ulong lane;
 	ulong qlm;
 
@@ -74,8 +129,12 @@ static int do_serdes(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	qlm = simple_strtoul(argv[1], NULL, 10);
 	lane = simple_strtoul(argv[2], NULL, 10);
 
-	cgx_intf_display_serdes(qlm, lane);
+	arm_smccc_smc(OCTEONTX_SERDES_DBG_GET_EYE, qlm, lane, 1, 0, 0, 0, 0, &res);
+	waitforresult(&res);
 
+        if (res.a0 != SMCCC_RET_SUCCESS) {
+                return CMD_RET_FAILURE;
+        }
 	return CMD_RET_SUCCESS;
 }
 

a7d9f4b25dc108eecc366bb27213023baba7580f
Suneel Garapati <sgarapati@marvell.com>
2020-11-26 11:46:00 -0800

    drivers: net: cn10k: Add check on pointer to free
    While flushing RX packets, add a check on pointer
    before calling free to NPA.
    
    Change-Id: Ib6615c956910b626067e2052459b27f3a3b0471e
    Signed-off-by: Suneel Garapati <sgarapati@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/u-boot/+/41140
    Tested-by: Devapraba Muthumani <dmuthumani@marvell.com>
    Reviewed-by: Devapraba Muthumani <dmuthumani@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/u-boot/+/42404
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: Suneel Garapati <sgarapati@caviumnetworks.com>

--- drivers/net/cn10k/nix.c
+++ drivers/net/cn10k/nix.c
@@ -605,7 +605,8 @@ void nix_lf_flush_rx(struct udevice *dev)
 
 		seg = (dma_addr_t *)(&rx_dr->rx_sg + 1);
 
-		npa_lf_rx_free_ptr(nix->npa, (u64)(*seg));
+		if (*seg)
+			npa_lf_rx_free_ptr(nix->npa, (u64)(*seg));
 
 		debug("%s return %llx to NPA\n", __func__, *seg);
 		nix_pf_reg_write(nix, NIXX_LF_CQ_OP_DOOR(),
